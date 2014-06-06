ebce0355310b9c37eb141936b255be9b49119bf2
fzhurd <rr1290@yahoo.com>
2014-06-05 23:20:03 -0700

    minor changes

--- PythonWork/checkIOCode/ATMquestion.py
+++ PythonWork/checkIOCode/ATMquestion.py
@@ -4,9 +4,7 @@
 
 
 def checkio(data):
-    #Your code here
-    #It's main function. Don't remove this function
-    #It's using for auto-testing and must return a result for check.  
+  
     balance, withdrawal = data
     l = len(withdrawal)
     
@@ -19,11 +17,7 @@ def checkio(data):
     
     return balance
 
-#Some hints:
-#Make sure you loop through the withdrawal amounts
-#import math (you'll need it for floor) or use int()
-#make sure you have enough money to withdraw,
-#otherwise don't (return the same balance)
+#automatic test
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':

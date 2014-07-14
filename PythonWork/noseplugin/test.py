0dc746857e46c2f5f827b43bdacb197f57568d32
Frank <frank@frank-VirtualBox.(none)>
2014-07-13 23:45:08 -0700

    changes

--- PythonWork/noseplugin/test.py
+++ PythonWork/noseplugin/test.py
@@ -1,3 +1,41 @@
 # usr/bin/python
+# -*- coding: utf-8 -*-
 
-print 'hello'
+import datetime
+import unittest
+import pymongo
+import subprocess
+import dateutil.parser
+import os
+from subprocess import call
+from multiprocessing import Process
+from threading import Thread
+from nose.tools import *
+from bson.son import SON
+import sqlalchemy
+from sqlalchemy import create_engine
+
+
+Number =10
+
+class FirstClass(object):
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def add(self):
+		global Number 
+		Number = Number +20
+
+
+firstClass = FirstClass('frank', 20)
+firstClass.add()
+
+
+print firstClass.name
+print Number
+print firstClass.__class__
+# print firstClass.__name__
+# print firstClass.__bases__
+print FirstClass
+print firstClass

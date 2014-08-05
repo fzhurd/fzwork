5b551e6f5312a886c1deade6f6a81dd9b4809329
Frank <frank@frank-VirtualBox.(none)>
2014-08-04 21:38:19 -0700

    setup script

---
+++ PythonWork/noseplugin/setup.py
@@ -0,0 +1,22 @@
+# usr/bin/python
+# -*- coding: utf-8 
+# try:
+#   from setuptools import setup, find_packages
+# except ImportError:
+#   import distribute_setup
+#   distribute_setup.use_setuptools()
+#   from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='application',
+    
+    install_requires=['nose.plugins.0.10'],
+    py_modules=['test-timer'],
+    entry_points={
+      'nose.plugins.0.10': [
+        'test-timer = test-timer:TestTimer'
+      ]
+    }
+)

d56f5db26a748c169de6d5cc39a79cb35977f6e9
Frank <frank@frank-VirtualBox.(none)>
2014-08-04 21:44:30 -0700

    changes

--- PythonWork/noseplugin/setup.py
+++ PythonWork/noseplugin/setup.py
@@ -1,13 +1,33 @@
 # usr/bin/python
 # -*- coding: utf-8 
-# try:
-#   from setuptools import setup, find_packages
-# except ImportError:
-#   import distribute_setup
-#   distribute_setup.use_setuptools()
-#   from setuptools import setup, find_packages
-from setuptools import setup
-from setuptools import find_packages
+
+import unittest
+import pymongo
+import subprocess
+import dateutil.parser
+import os
+from subprocess import call
+from multiprocessing import Process
+from threading import Thread
+from nose.tools import *
+
+import sqlalchemy
+from sqlalchemy import create_engine
+
+import logging
+
+import nose
+from nose.plugins.base import Plugin
+
+
+try:
+  from setuptools import setup, find_packages
+except ImportError:
+  import distribute_setup
+  distribute_setup.use_setuptools()
+  from setuptools import setup, find_packages
+# from setuptools import setup
+# from setuptools import find_packages
 
 setup(
     name='application',

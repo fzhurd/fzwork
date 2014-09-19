b783ee93d4cd1826c79a61e40eed53b983543857
fzhurd <minganghz@gmail.com>
2014-09-18 21:15:44 -0700

    minor changes

--- Linux/shellScript/replace1.sh
+++ Linux/shellScript/replace1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash  
 
-directory = /usr/bin
+directory=/usr/bin
 
-fs = "Free Software Foundation"
+fs="Free Software Foundation"
 
-for f $( find $directory -type f -name '*' | sort )
+for f $( `find $directory -type f -name '*' | sort` )
 do
 	strings -f $file | grep "$fstring" | sed -e "s%$directory%%"
 done

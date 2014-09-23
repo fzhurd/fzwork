c6c2077d2b38cacb207f180232530a5367b5a430
fzhurd <minganghz@gmail.com>
2014-09-22 21:05:07 -0700

    test codes

---
+++ PythonWork/test2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import datetime
+import unittest
+import pymongo
+import subprocess
+import re
+import dateutil.parser
+import sys
+import tempfile
+import os
+import argparse
+from bson.objectid import ObjectId
+from bson.timestamp import Timestamp
+from bson.binary import Binary
+from subprocess import call
+from multiprocessing import Process
+from bson.code import Code
+from threading import Thread
+from bson.son import SON
+
+def main():
+	print 'hi'
+
+
+if __name__=="__main__":
+	main()

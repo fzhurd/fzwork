2bd677e8ae38ce785ec73911d00046cdad075c01
fzhurd <minganghz@gmail.com>
2014-09-26 22:16:44 -0700

    tempfile module test

--- PythonWork/test_temp.py
+++ PythonWork/test_temp.py
@@ -12,5 +12,49 @@ import socket
 import sys
 import time
 import uuid
+import tempfile
 
 from pymongo import MongoClient
+
+temp = tempfile.TemporaryFile()
+# temp2 = tempfile.TemporaryFile()
+
+# temp.write('Some data')
+# temp.seek(0)
+
+print temp
+print temp.name
+print temp.read()
+
+t1 = temp.read()
+print t1
+
+print '****************************'
+
+# print temp2
+# print temp2.read()
+
+# t2 = temp2.read()
+
+# print os.path.getsize(temp.name)
+# print os.path.getsize(temp2.name)
+
+
+if  not t1:
+	print True
+	
+else:
+	print False
+
+# if t2 is not None:
+# 	print True
+# else:
+# 	print False
+
+
+    # print temp.name
+    # print os.path.cwd()
+
+    # if os.path.getsize('<fdopen>') >0:
+    # 	print 'hi'
+

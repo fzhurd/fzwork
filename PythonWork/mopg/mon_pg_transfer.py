81041b63ba6e5a4019586c816a013508015693a5
fzhurd <minganghz@gmail.com>
2014-10-05 00:03:32 -0700

    more changes

--- PythonWork/mopg/mon_pg_transfer.py
+++ PythonWork/mopg/mon_pg_transfer.py
@@ -24,6 +24,36 @@ import pymongo
 from pymongo import MongoClient
 
 
+def setup_pg_connection(postgres_db_url, db='test'):
+
+    global connectionPsql
+
+    postgres_db_url = postgres_db_url +db
+
+    engine = create_engine(postgres_db_url)
+
+    engine.echo = True
+
+    connectionPsql = engine.connect()
+
+    connectionPsql.connection.connection.set_isolation_level(0)
+
+
+
+def setup_mongo_connection(mongodb = 'test'):
+
+	dbconnection = pymongo.MongoClient('localhost', 27017)
+	db = dbconnection[mongodb]
+
+	return db
+
+
+
+def mongo_to_pg_transfer(mongo_db_name, mongo_collection_name, pg_db_name, pg_collection_name):
+	pass
+
+
+
 def read_in_the_field_names(file_name):
 	with open(file_name) as fhandler:
 		for i in fhandler:

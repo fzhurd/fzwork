2052f4ba1d0d02bba7dfe66ce0e827a4aaef24cb
fzhurd <minganghz@gmail.com>
2014-10-17 23:03:33 -0700

    awk example

--- Linux/shellScript/awkText1.sh
+++ Linux/shellScript/awkText1.sh
@@ -1,7 +1,7 @@
 
 
-#
-mongorestore  -d test -c collection_name --directoryperdb  ./dump
+
+# mongorestore  -d test -c collection_name --directoryperdb  ./dump
 
 awk -F '|' '{print $1}' file1.txt >file2.txt
 awk '$1=$1' file2.txt > file3.txt
@@ -10,3 +10,6 @@ awk -F '|' '{print $1 $2 ","}' file1.txt >file12.txt
 awk '$1=$1' file12.txt > file15.txt
 
 awk -F '|' '{print $1 $2}' file1.txt | awk '{print $0,","}' >file11.txt
+
+awk -F '|' '{print $1 $2 }' file1.txt >file16.txt
+awk '$1=$1' file16.txt > file18.txt

f731e24ae3a01203bfa4422b9d7e3d2adb9f5ff2
fzhurd <minganghz@gmail.com>
2014-11-03 21:00:08 -0800

    add logger

--- PythonWork/mopg/logger.py
+++ PythonWork/mopg/logger.py
@@ -23,9 +23,24 @@ from sqlalchemy.orm import sessionmaker
 import pymongo
 from pymongo import MongoClient
 
+import logging
+
 
 def main():
-    pass
+    # 使用一个名字为fib的logger
+    logger = logging.getLogger('fib')
+
+    # 设置logger的level为DEBUG
+    logger.setLevel(logging.DEBUG)
+
+    # 创建一个输出日志到控制台的StreamHandler
+    hdr = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s] %(name)s:%(levelname)s: %(message)s')
+    hdr.setFormatter(formatter)
+
+    # 给logger添加上handler
+    logger.addHandler(hdr)
+
 
 
 

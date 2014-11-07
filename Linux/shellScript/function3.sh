d43b4df66a641ca9b81c22afa2df38fe803d20b8
fzhurd <minganghz@gmail.com>
2014-11-06 21:28:16 -0800

    add more exercises

--- Linux/shellScript/function3.sh
+++ Linux/shellScript/function3.sh
@@ -15,3 +15,29 @@ function fSum()
 
  total3=$(fSum 12 20);
  echo $total3,$?;
+
+
+
+echo $(uname);
+declare num=1000;
+ 
+uname()
+{
+    echo "test!";
+    ((num++));
+    return 100;
+}
+testvar()
+{
+    local num=10;
+    ((num++));
+    echo $num;
+ 
+}
+ 
+uname;
+echo $?
+echo $num;
+testvar;
+echo $num;
+          

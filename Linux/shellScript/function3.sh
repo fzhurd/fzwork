f3cd242807b670e357dfeee208a66367497c6b4f
fzhurd <minganghz@gmail.com>
2014-11-06 21:15:29 -0800

    more examples

--- Linux/shellScript/function3.sh
+++ Linux/shellScript/function3.sh
@@ -6,8 +6,12 @@ function fSum()
      return $(($1+$2));
  }
  fSum 5 7;
+ echo '***************************'
  total=$(fSum 3 2);
  echo $total,$?;
-
+ echo '***************************8'
  total2=$(fSum 300 200);
  echo $total2,$?;
+
+ total3=$(fSum 12 20);
+ echo $total3,$?;

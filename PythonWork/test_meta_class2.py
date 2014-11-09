2ba9a03d5c0be282aa87518cc76c0cf8d2bf18ff
fzhurd <minganghz@gmail.com>
2014-11-09 13:44:51 -0800

    decorator function

---
+++ PythonWork/test_meta_class2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+import os
+
+
+def entryExit(f):
+    def new_f():
+        print "Entering", f.__name__
+        f()
+        print "Exited", f.__name__
+    return new_f
+ 
+@entryExit
+def func1():
+    print "inside func1()"
+ 
+@entryExit
+def func2():
+    print "inside func2()"
+ 
+func1()
+func2()
+print func1.__name__

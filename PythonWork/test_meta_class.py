a2a600c76417cd4116d0e8c60ed5865a6600d500
fzhurd <minganghz@gmail.com>
2014-11-09 13:34:55 -0800

    class decorator

--- PythonWork/test_meta_class.py
+++ PythonWork/test_meta_class.py
@@ -51,7 +51,18 @@ class ID3v2CommentFrame(ID3v2Frame):
     frame_identifier = "COMM"
  
 
-
+class myDecorator(object):
+ 
+    def __init__(self, f):
+        print "inside myDecorator.__init__()"
+        f() # Prove that function definition has completed
+ 
+    def __call__(self):
+        print "inside myDecorator.__call__()"
+ 
+@myDecorator
+def aFunction():
+    print "inside aFunction()"
 
 def main():
 	c = C()

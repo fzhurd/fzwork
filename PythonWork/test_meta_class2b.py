21a37cb427aa0fd1ae7a4c76e80e5fbe712b7e6b
fzhurd <minganghz@gmail.com>
2014-11-11 20:10:48 -0800

    minor changes

--- PythonWork/test_meta_class2b.py
+++ PythonWork/test_meta_class2b.py
@@ -23,4 +23,18 @@ def func2():
  
 func1()
 func2()
+
+
+def outer(some_func):
+    def inner():
+        print "before some_func"
+        ret = some_func() 
+        return ret + 1
+        return inner
+
+def foo():
+    return 1
+
+decorated = outer(foo) 
+decorated()
  

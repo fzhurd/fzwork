1620fbe92d435e8cc16be0ed8f77dcab6a95645b
fzhurd <minganghz@gmail.com>
2014-11-14 20:41:10 -0800

    changes

--- Linux/shellScript/forloop3.sh
+++ Linux/shellScript/forloop3.sh
@@ -41,4 +41,52 @@ do
  echo $i  
  let "i+=1"   #加1  
 done  
+exit 0  #!/bin/bash  
+  
+echo "Input a number #1 "  
+read num  
+echo "variable #1 = $num"  
+  
+if [ $num -lt 60 ]    #注意lt前面的-，很容易忘的  
+then  
+ echo "you are not pass"  
+elif [ $num -lt 70 ] && [ $num -ge 60 ]  #多个条件的判断  
+then  
+ echo "pass"  
+elif [[ $num -lt 85 && $num -ge 70 ]] #如果放在一起，要注意是双方括号，不要写成[ $num -lt 85 && $num -ge 70 ]  
+then  
+ echo "good"  
+elif (( $num <= 100 )) && (( $num >= 85 ))  #对于有语言基础的人来说，这种写法让人觉得很舒服，不要忘了是双小括号  
+then  
+ echo "very good"  
+else  
+ echo "num is wrong"  
+fi                                 #if要有结束标签的，根XML很像,不闭合，就报错  
+  
+exit 0  
+
+
+
+#!/bin/bash  
+  
+echo "Input a number #1 "  
+read num  
+echo "variable #1 = $num"  
+  
+if [ $num -lt 60 ]    #注意lt前面的-，很容易忘的  
+then  
+ echo "you are not pass"  
+elif [ $num -lt 70 ] && [ $num -ge 60 ]  #多个条件的判断  
+then  
+ echo "pass"  
+elif [[ $num -lt 85 && $num -ge 70 ]] #如果放在一起，要注意是双方括号，不要写成[ $num -lt 85 && $num -ge 70 ]  
+then  
+ echo "good"  
+elif (( $num <= 100 )) && (( $num >= 85 ))  #对于有语言基础的人来说，这种写法让人觉得很舒服，不要忘了是双小括号  
+then  
+ echo "very good"  
+else  
+ echo "num is wrong"  
+fi                                 #if要有结束标签的，根XML很像,不闭合，就报错  
+  
 exit 0  

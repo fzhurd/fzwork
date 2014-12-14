877a7f04c1e8b96433d88cafc57e16ba2ea02af8
fzhurd <minganghz@gmail.com>
2014-12-13 23:33:53 -0800

    kerbos auth

---
+++ PythonWork/test_kerbos.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python 
+
+import argparse
+import bson
+import datetime
+import logging
+import os
+import pymongo
+import random
+import random
+import socket
+import sys
+import time
+import uuid
+import tempfile
+#import PDB
+
+from pymongo import MongoClient
+
+def main():
+	pass
+
+
+if __name__=='__maind__':
+	maind()

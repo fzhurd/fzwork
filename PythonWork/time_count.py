1036534ed6155bee81c6584636d66f02b6731041
fzhurd <minganghz@gmail.com>
2014-12-14 21:46:05 -0800

    changes

--- PythonWork/time_count.py
+++ PythonWork/time_count.py
@@ -13,29 +13,41 @@ import sys
 import time
 import uuid
 import tempfile
-import kerberos
+# import kerberos
+import time
+from functools import wraps
 #import PDB
 
 from pymongo import MongoClient
 
-def main():
-	print 'hi'
 
-import time
-from functools import wraps
-def timethis(func):
+
 '''
 Decorator that reports the execution time.
 '''
-@wraps(func)
-def wrapper(*args, **kwargs):
-	start = time.time()
-result = func(*args, **kwargs)
-end = time.time()
-print(func.__name__, end-start)
-return result
-return wrapper
+def timethis(func):
+
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		start = time.time()
+		result = func(*args, **kwargs)
+		end = time.time()
+		print(func.__name__, end-start)
+		return result
+	return wrapper
+
+@timethis
+def countdown(num):
+	while num>0:
+		num= num-1
 
 
-if __name__=='__maind__':
-	maind()
+def main():
+	countdown(100)
+
+
+
+
+
+if __name__=='__main__':
+	main()

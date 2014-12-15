ad594a220819b5f793a0b809e826708d4c9b8898
fzhurd <minganghz@gmail.com>
2014-12-14 21:34:40 -0800

    time count decorator

---
+++ PythonWork/time_count.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python 
+
+import argparse
+import bson
+import datetime
+import logging
+import os
+import pymongo
+import random
+import random
+import socket
+import sys
+import time
+import uuid
+import tempfile
+import kerberos
+#import PDB
+
+from pymongo import MongoClient
+
+def main():
+	print 'hi'
+
+
+if __name__=='__maind__':
+	maind()

e653619eb99c606456dbe4f96ab378c31e57ba5b
fzhurd <minganghz@gmail.com>
2014-12-14 21:36:18 -0800

    time count decorator2

--- PythonWork/time_count.py
+++ PythonWork/time_count.py
@@ -21,6 +21,21 @@ from pymongo import MongoClient
 def main():
 	print 'hi'
 
+import time
+from functools import wraps
+def timethis(func):
+'''
+Decorator that reports the execution time.
+'''
+@wraps(func)
+def wrapper(*args, **kwargs):
+	start = time.time()
+result = func(*args, **kwargs)
+end = time.time()
+print(func.__name__, end-start)
+return result
+return wrapper
+
 
 if __name__=='__maind__':
 	maind()

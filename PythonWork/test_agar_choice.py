723c0e9e97539d6d3e2cdd5bbce9fda8fa206f03
fzhurd <minganghz@gmail.com>
2014-12-15 21:18:03 -0800

    agar exercise

---
+++ PythonWork/test_agar_choice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+import os
+
+
+
+
+def main():
+
+	parser = argparse.ArgumentParser(prog='roshambo.py')
+	parser.add_argument('throw', choices=['rock', 'paper', 'scissors'])
+	args = parser.parse_args()
+ 
+print("~ Throw: {}".format(args.throw))
+
+	pass
+
+if __name__ == '__main__':
+	main()

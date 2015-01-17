2242eeb3b052760cadc46a6bc21236135c63d3aa
fzhurd <minganghz@gmail.com>
2015-01-16 20:36:18 -0800

    shell codes

--- Linux/shellScript/ex1.sh
+++ Linux/shellScript/ex1.sh
@@ -5,5 +5,8 @@ read score
 echo $score
 
 if [ "$score" -lt 0 -o "$score" -gt 100 ]
-	then 
-		echo "the score what you inpt is not integer or the is not in 0 and 100"
+	then echo "the score what you inpt is not integer or the is not in 0 and 100"
+elif [ "$score" -ge 90 ]
+	then echo "A"
+
+fi

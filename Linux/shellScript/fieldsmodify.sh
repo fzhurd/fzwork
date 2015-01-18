7e0aa6cefe436e9ac1905b5411b3c705a1568c3c
fzhurd <minganghz@gmail.com>
2015-01-18 10:37:24 -0800

    fields changes

---
+++ Linux/shellScript/fieldsmodify.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+awk -F '|' '{print $1}' tweetsfields1 > tweetsfields2
+awk '{print $1=$1}' tweetsfields2 >tweetsfields3
+awk '{print "\""$1"\""}' tweetsfields3 >tweetsfields4
+
+awk '{print $1, ","}' tweetsfields4 > tweetsfields5
+
+awk '{{printf"%s",$0}}' tweetsfields5 > tweetsfields6

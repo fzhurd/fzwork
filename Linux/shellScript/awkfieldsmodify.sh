681588584f0d7ec7f7fae4c1fbd44a7ee7df873a
fzhurd <minganghz@gmail.com>
2015-01-18 10:38:26 -0800

    added

--- Linux/shellScript/awkfieldsmodify.sh
+++ Linux/shellScript/awkfieldsmodify.sh
@@ -6,3 +6,5 @@ awk '{print "\""$1"\""}' tweetsfields3 >tweetsfields4
 awk '{print $1, ","}' tweetsfields4 > tweetsfields5
 
 awk '{{printf"%s",$0}}' tweetsfields5 > tweetsfields6
+
+cat tweetsfields5 | xargs echo

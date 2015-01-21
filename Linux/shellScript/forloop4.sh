193dc5f0e18be4a071d3a7b4e9f1fc194c2cad12
fzhurd <minganghz@gmail.com>
2015-01-20 19:44:19 -0800

    added

--- Linux/shellScript/forloop4.sh
+++ Linux/shellScript/forloop4.sh
@@ -11,3 +11,22 @@ do
 	echo ""
 
 done
+
+
+
+for (( i=1; i<=8; i++ ))
+do
+	for (( j=1; j<=8; j++ ))
+	do
+		total =$(( $i + $j))
+		tmp = $(( $total % 2 ))
+
+		if [ $tmp -eq 0 ];
+		then
+			echo -e -n "\033[47m"
+		else
+				echo -e -n "\033[40m"
+		fi
+	done
+	echo ""
+done

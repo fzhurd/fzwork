e69ccb84bef12debc0c8df0c4ff92828390dd19f
fzhurd <minganghz@gmail.com>
2015-01-20 19:51:47 -0800

    nested loop codes

--- Linux/shellScript/forloop4.sh
+++ Linux/shellScript/forloop4.sh
@@ -18,14 +18,14 @@ for (( i=1; i<=8; i++ ))
 do
 	for (( j=1; j<=8; j++ ))
 	do
-		total =$(( $i + $j))
-		tmp = $(( $total % 2 ))
+		total=$(( $i + $j ))
+		tmp=$(( $total % 2 ))
 
 		if [ $tmp -eq 0 ];
 		then
-			echo -e -n "\033[47m"
+			echo -e -n "\033[47m "
 		else
-				echo -e -n "\033[40m"
+			echo -e -n "\033[40m "
 		fi
 	done
 	echo ""

f5fb4cc688b9b418d3d5de41433921f63f10b588
fzhurd <minganghz@gmail.com>
2015-02-01 20:07:56 -0800

    added codes

--- Linux/shellScript/awkText1.sh
+++ Linux/shellScript/awkText1.sh
@@ -12,4 +12,7 @@ awk '$1=$1' file12.txt > file15.txt
 awk -F '|' '{print $1 $2}' file1.txt | awk '{print $0,","}' >file11.txt
 
 awk -F '|' '{print $1 $2 }' file1.txt >file16.txt
+
 awk '$1=$1' file16.txt > file18.txt
+
+awk '$1=$1' file16.txt > file19.txt

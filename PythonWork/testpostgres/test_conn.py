b1ff0c0e330b0e3f93a9ab22f4c544271c9adabe
fzhurd <minganghz@gmail.com>
2015-02-06 20:15:39 -0800

    add psycopg2 connection

--- PythonWork/testpostgres/test_conn.py
+++ PythonWork/testpostgres/test_conn.py
@@ -6,7 +6,11 @@ import pymongo
 def main():
 	print 'hi'
 
-
+def set_up_pg_connection(pg_user, pg_password, pg_host, pg_port=5432, pg_db='postgres'):
+    connection_string = "host='{}' port='{}' dbname='{}' user='{}' password='{}'".format(
+        pg_host, pg_port, pg_db, pg_user, pg_password)
+    connection_psql=psycopg2.connect(connection_string)
+    return connection_psql
 
 if __name__=='__main__':
 	main()

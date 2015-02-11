e75cd6d16f230bd166bf35be5ffbbb02beac6fd8
fzhurd <minganghz@gmail.com>
2015-02-10 20:36:33 -0800

    add command line

--- PythonWork/testpostgres/test_conn.py
+++ PythonWork/testpostgres/test_conn.py
@@ -22,15 +22,20 @@ def main():
     pg_port = cf.get('postgres', 'postgres_server_port')
     pg_db = cf.get('postgres', 'postgres_db_name')
 
-
-    perform_sql_query('select * from test1', postgres_user, postgres_password,pg_host,pg_port,pg_db)
+    perform_sql_query('select * from test1', postgres_user, pg_host,pg_port,pg_db)
+    # perform_sql_query('select * from test1', postgres_user, postgres_password,pg_host,pg_port,pg_db)
     # perform_sql_query('select * from test1', 'test', 'test','127.0.0.1',5432,'test')
 
-def set_up_pg_connection(pg_user, pg_password, pg_host, pg_port=5432, pg_db='postgres'):
+# def set_up_pg_connection(pg_user, pg_password, pg_host, pg_port=5432, pg_db='postgres'):
+def set_up_pg_connection(pg_user, pg_host, pg_port=5432, pg_db='postgres'):
     try:
         # subprocess.check_call('sudo -i -u postgres' , shell=True)
-        subprocess.check_call('sudo su - postgres' , shell=True)
-        subprocess.check_call('psql -U postgres -d postgres' , shell=True)
+        # subprocess.check_call('sudo su - postgres' , shell=True)
+        subprocess.check_call('sudo -u postgres psql -c "ALTER USER postgres PASSWORD \'postgres\';"' , shell=True)
+        connection_string = "host='{}' port='{}' dbname='{}' user='{}' ".format(
+        pg_host, pg_port, 'postgres', 'postgres')
+
+        # subprocess.check_call('psql -U postgres -d postgres' , shell=True)
         # connection_string = "host='{}' port='{}' dbname='{}' user='{}' password='{}'".format(
         # pg_host, pg_port, pg_db, pg_user, pg_password)
         
@@ -50,7 +55,8 @@ def set_up_pg_connection(pg_user, pg_password, pg_host, pg_port=5432, pg_db='pos
 
 def perform_sql_query(pg_query, pg_user, pg_password, pg_host, pg_port=5432, pg_db='test' ):
 
-    connection_psql = set_up_pg_connection(pg_user, pg_password, pg_host, pg_port, pg_db)
+    # connection_psql = set_up_pg_connection(pg_user, pg_password, pg_host, pg_port, pg_db)
+    connection_psql = set_up_pg_connection(pg_user, pg_host, pg_port, pg_db)
 
     result_source_cursor = connection_psql.cursor('my_cursor')
     # pg_query = 'select * from test1;'

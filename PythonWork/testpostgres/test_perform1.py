de36442dcf87da704aa54878545882bd8fa65d36
fzhurd <minganghz@gmail.com>
2015-02-15 18:11:25 -0800

    add codes

---
+++ PythonWork/testpostgres/test_perform1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+import os
+import pymongo
+import psycopg2
+import ConfigParser
+import subprocess
+
+
+def main():
+    print 'hi'
+
+if __name__=='__main__':
+    main()

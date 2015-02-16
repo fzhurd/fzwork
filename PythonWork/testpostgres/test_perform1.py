91f113eed2cfa6ebe14336eae79129a856c75168
fzhurd <minganghz@gmail.com>
2015-02-15 18:17:17 -0800

    add codes

--- PythonWork/testpostgres/test_perform1.py
+++ PythonWork/testpostgres/test_perform1.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
-import sys
+# -*- coding: utf-8 -*-
+import argparse
+import datetime
 import os
 import pymongo
-import psycopg2
-import ConfigParser
+import sys
+import time
+from pymongo import MongoClient
 import subprocess
+
+
+def perform_mongo_import(port, db_name='test', file_name='file.json'):
+    start = time.time()
+    print 'import start time:', start
+    subprocess.call('mongoimport --port %d -d %s --file %s --drop' % (port, db_name, file_name), shell=True)
+    end = time.time()
+    print 'import end time:', end
+    import_used_time = end - start
+    print 'import use time', import_used_time
+    return import_used_time
+
+def perform_mongo_export(port, db_name='test', collection_name= 'test', file_name='file.json'):
+    start = time.time()
+    print 'export start time:', start
+    subprocess.call('mongoexport --port %d -d %s -c %s -o %s' % (port,
+    db_name, collection_name, file_name), shell=True)
+    end = time.time()
+    print 'export end time:', end
+    export_used_time = end - start
+    print 'import use time', export_used_time
+    return export_used_time
 
 
 def main():

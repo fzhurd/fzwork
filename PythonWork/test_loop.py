fbed79b9fb78bc2df44f4904e568b361fb52b1c1
fzhurd <minganghz@gmail.com>
2015-03-08 19:11:59 -0700

    add

---
+++ PythonWork/test_loop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import argparse
+import datetime
+import os
+import pymongo
+import sys
+import time
+from pymongo import MongoClient
+import subprocess
+
+
+
+    
+def main():
+   pass
+
+if __name__=='__main__':
+    main()

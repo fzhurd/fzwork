bdeedc497c258228be1df51c516a511b312fe20c
fzhurd <minganghz@gmail.com>
2015-03-08 19:14:03 -0700

    add2

--- PythonWork/test_loop.py
+++ PythonWork/test_loop.py
@@ -10,10 +10,20 @@ from pymongo import MongoClient
 import subprocess
 
 
+def test_dict():
+
+    dic_eg1 = [({'a':1, 'b':1, 'c':1}, {'aa':11, 'bb':11, 'cc':11}),( {'a':10, 'b':10, 'c':10},),({'a':20, 'b':20, 'c':30},)]
+    # dic_eg1 = [( {'aa':11, 'bb':11, 'cc':11}),({'a':10, 'b':10, 'c':10}),({'a':20, 'b':20, 'c':30})]
+    # a = {'aa':11, 'bb':11, 'cc':11}
+    # print type(a)
+    for i in dic_eg1:
+        print type(i), i
+        for x in xrange(0, len(i)):
+            print i[x]
 
     
 def main():
-   pass
+   test_dict()
 
 if __name__=='__main__':
     main()

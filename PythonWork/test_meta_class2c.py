c8dec0d7f9770cae974d9ca088bae518a1961203
fzhurd <minganghz@gmail.com>
2015-03-18 20:22:13 -0700

    meta class test

---
+++ PythonWork/test_meta_class2c.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import subprocess
+import time
+from functools import wraps
+import pymongo
+from pymongo import MongoClient
+from memory_profiler import memory_usage
+import threading
+import psutil
+import os
+
+
+def main():
+    print 'hi'
+
+
+if __name__=='__main__':
+    main()
+
+
+
+
+

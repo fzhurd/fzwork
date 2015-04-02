c301477faf27bd7fe09e5469db47e1932ca70dfc
fzhurd <minganghz@gmail.com>
2015-04-01 20:34:21 -0700

    add meta class

--- PythonWork/test_meta_class2c.py
+++ PythonWork/test_meta_class2c.py
@@ -43,6 +43,38 @@ class MyKlass(object):
 
 MyKlass = type(name, bases, dct)
 
+# the metaclass will automatically get passed the same argument
+# that you usually pass to `type`
+def upper_attr(future_class_name, future_class_parents, future_class_attr):
+  """
+    Return a class object, with the list of its attribute turned
+    into uppercase.
+  """
+
+  # pick up any attribute that doesn't start with '__'
+  attrs = ((name, value) for name, value in future_class_attr.items() if not name.startswith('__'))
+  # turn them into uppercase
+  uppercase_attr = dict((name.upper(), value) for name, value in attrs)
+
+  # let `type` do the class creation
+  return type(future_class_name, future_class_parents, uppercase_attr)
+
+__metaclass__ = upper_attr # this will affect all classes in the module
+
+class Foo(): # global __metaclass__ won't work with "object" though
+  # but we can define __metaclass__ here instead to affect only this class
+  # and this will work with "object" children
+  bar = 'bip'
+
+print hasattr(Foo, 'bar')
+# Out: False
+print hasattr(Foo, 'BAR')
+# Out: True
+
+f = Foo()
+print f.BAR
+# Out: 'bip'
+
 
 if __name__=='__main__':
     main()

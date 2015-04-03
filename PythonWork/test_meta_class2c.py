08997176030d41ecf37c9fe72381e35b75ebde38
fzhurd <minganghz@gmail.com>
2015-04-02 21:06:52 -0700

    more meta

--- PythonWork/test_meta_class2c.py
+++ PythonWork/test_meta_class2c.py
@@ -68,6 +68,27 @@ f = Foo()
 print f.BAR, '************************8'
 # Out: 'bip'
 
+# remember that `type` is actually a class like `str` and `int`
+# so you can inherit from it
+class UpperAttrMetaclass(type):
+    # __new__ is the method called before __init__
+    # it's the method that creates the object and returns it
+    # while __init__ just initializes the object passed as parameter
+    # you rarely use __new__, except when you want to control how the object
+    # is created.
+    # here the created object is the class, and we want to customize it
+    # so we override __new__
+    # you can do some stuff in __init__ too if you wish
+    # some advanced use involves overriding __call__ as well, but we won't
+    # see this
+    def __new__(upperattr_metaclass, future_class_name,
+                future_class_parents, future_class_attr):
+
+        attrs = ((name, value) for name, value in future_class_attr.items() if not name.startswith('__'))
+        uppercase_attr = dict((name.upper(), value) for name, value in attrs)
+
+        return type(future_class_name, future_class_parents, uppercase_attr)
+
 
 if __name__=='__main__':
     main()

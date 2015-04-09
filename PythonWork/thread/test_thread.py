7915e3c3e72f4ce259c66781499ce6527a050046
fzhurd <minganghz@gmail.com>
2015-04-08 20:46:45 -0700

    initial thread

--- PythonWork/thread/test_thread.py
+++ PythonWork/thread/test_thread.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import threading
+
+
+class MyThread(threading.Thread):
+    def __init__(self, event):
+        threading.Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(0.5):
+            print "my thread"
+      
+
 
 def main():
-    print 'hi'
+    stopFlag = threading.Event()
+    thread = MyThread(stopFlag)
+    thread.start()
+    stopFlag.set()
 
 
 if __name__=='__main__':

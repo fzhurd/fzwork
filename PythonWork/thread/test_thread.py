f4f1e1ec9ccf62463f19e7186e14b92d37a96995
fzhurd <minganghz@gmail.com>
2015-04-09 20:52:38 -0700

    more classes

--- PythonWork/thread/test_thread.py
+++ PythonWork/thread/test_thread.py
@@ -12,8 +12,17 @@ class MyThread(threading.Thread):
     def run(self):
         while not self.stopped.wait(0.5):
             print "my thread"
-      
 
+class MyThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(0.5):
+            print "my thread"
+            # call a function
+      
 
 def main():
     stopFlag = threading.Event()

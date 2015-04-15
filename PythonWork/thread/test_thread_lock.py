d40db8f9f5f8554bd3e45ee61e4f62a51de8b305
fzhurd <minganghz@gmail.com>
2015-04-14 22:01:40 -0700

    thread lock and sephamore

--- PythonWork/thread/test_thread_lock.py
+++ PythonWork/thread/test_thread_lock.py
@@ -24,3 +24,21 @@ t3 = threading.Thread(target=func, args=(2,))
 t1.start()
 t2.start()
 t3.start()
+
+
+import threading
+import time 
+
+semaphore = threading.Semaphore(2)
+ 
+def func():
+    if semaphore.acquire():
+        for i in range(5):
+          print (threading.currentThread().getName() + ' get semaphore')
+        semaphore.release()
+        print (threading.currentThread().getName() + ' release semaphore')
+        
+        
+for i in range(4):
+  t1 = threading.Thread(target=func)
+  t1.start()

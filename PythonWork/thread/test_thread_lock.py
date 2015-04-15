9d4f5e0376617ef471c5b8a29f7b4aaca31afa75
fzhurd <minganghz@gmail.com>
2015-04-14 22:04:29 -0700

    communication between two threads

--- PythonWork/thread/test_thread_lock.py
+++ PythonWork/thread/test_thread_lock.py
@@ -42,3 +42,34 @@ def func():
 for i in range(4):
   t1 = threading.Thread(target=func)
   t1.start()
+
+
+
+
+import logging
+import threading
+import time
+
+logging.basicConfig(level=logging.DEBUG,
+format="(%(threadName)-10s : %(message)s",
+)
+
+def wait_for_event_timeout(e, t):
+    """Wait t seconds and then timeout"""
+    while not e.isSet():
+      logging.debug("wait_for_event_timeout starting")
+      event_is_set = e.wait(t)
+      logging.debug("event set: %s" % event_is_set)
+    if event_is_set:
+      logging.debug("processing event")
+    else:
+      logging.debug("doing other work")
+      
+e = threading.Event()
+t2 = threading.Thread(name="nonblock",
+target=wait_for_event_timeout,args=(e, 2))
+t2.start()
+logging.debug("Waiting before calling Event.set()")
+time.sleep(7)
+e.set()
+logging.debug("Event is set")

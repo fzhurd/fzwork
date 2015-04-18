80f12f243ebb93ceb7a04c0a8fa3631861bcb78d
fzhurd <minganghz@gmail.com>
2015-04-18 15:30:28 -0700

    add example signal3

--- PythonWork/thread/test_signal3.py
+++ PythonWork/thread/test_signal3.py
@@ -5,8 +5,34 @@ import signal
 import time
 import threading
 
+
+def signal_handler(num, stack):
+    print time.ctime(), 'Alarm in', threading.currentThread()
+
+
+def use_alarm():
+    print time.ctime(), 'Setting alarm in', threading.currentThread()
+    signal.alarm(10)
+    print time.ctime(), 'Sleeping in', threading.currentThread()
+    # time.sleep(3)
+    print time.ctime(), 'Done with sleep'
+
+
+def keep_run():
+
+  i=0
+  while 1:
+    print i
+    i=i+1
+    time.sleep(1)
+
 def main():
-  print 'hi'
+  alarm_thread = threading.Thread(target=use_alarm, name='alarm_thread') 
+  alarm_thread.start()
+  # time.sleep(0.1)
+  
+  keep_run()
+  signal.signal(signal.SIGALRM, signal_handler)
 
 if __name__=='__main__':
   main()

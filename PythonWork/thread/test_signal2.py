75964f7035616e459329b988fc4c6e0e77763813
fzhurd <minganghz@gmail.com>
2015-04-18 14:57:41 -0700

    signal2

---
+++ PythonWork/thread/test_signal2.py
@@ -0,0 +1,35 @@
+import signal
+import time
+import threading
+
+
+def signal_handler(num, stack):
+    print time.ctime(), 'Alarm in', threading.currentThread()
+
+
+
+signal.signal(signal.SIGALRM, signal_handler)
+
+
+
+def use_alarm():
+    print time.ctime(), 'Setting alarm in', threading.currentThread()
+    signal.alarm(1)
+    print time.ctime(), 'Sleeping in', threading.currentThread()
+    time.sleep(3)
+    print time.ctime(), 'Done with sleep'
+
+
+
+alarm_thread = threading.Thread(target=use_alarm, name='alarm_thread') #启动一个线程，将不会收到信号
+alarm_thread.start()
+time.sleep(0.1)
+
+
+
+print time.ctime(), 'Waiting for', alarm_thread
+alarm_thread.join()
+
+
+
+print time.ctime(), 'Exiting normally'

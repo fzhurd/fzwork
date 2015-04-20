84421989eae4897f36e53f95997b1e075407f010
fzhurd <minganghz@gmail.com>
2015-04-19 20:23:55 -0700

    initial start

---
+++ PythonWork/thread/test_thread_lock2.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import threading
+import time
+import logging
+
+def main():
+  print 'hi'
+
+  
+
+if __name__=='__main__':
+  main()
+ 
+# num = 0
+# lock = threading.Lock()
+ 
+# def func(st):
+#     global num
+#     print (threading.currentThread().getName() + ' try to acquire the lock')
+#     if lock.acquire():
+#         print (threading.currentThread().getName() + ' acquire the lock.' )
+#         print (threading.currentThread().getName() +" :%s" % str(num) )
+#         num += 1
+#         time.sleep(st)
+#         print (threading.currentThread().getName() + ' release the lock.'  )        
+#         lock.release()
+ 
+# t1 = threading.Thread(target=func, args=(8,))
+# t2 = threading.Thread(target=func, args=(4,))
+# t3 = threading.Thread(target=func, args=(2,))
+# t1.start()
+# t2.start()
+# t3.start()
+
+
+
+# semaphore = threading.Semaphore(2)
+ 
+# def func():
+#     if semaphore.acquire():
+#         for i in range(5):
+#           print (threading.currentThread().getName() + ' get semaphore')
+#         semaphore.release()
+#         print (threading.currentThread().getName() + ' release semaphore')
+        
+        
+# for i in range(4):
+#   t1 = threading.Thread(target=func)
+#   t1.start()
+
+
+# logging.basicConfig(level=logging.DEBUG,
+# format="(%(threadName)-10s : %(message)s",
+# )
+
+# def wait_for_event_timeout(e, t):
+#     """Wait t seconds and then timeout"""
+#     while not e.isSet():
+#       logging.debug("wait_for_event_timeout starting")
+#       event_is_set = e.wait(t)
+#       logging.debug("event set: %s" % event_is_set)
+#     if event_is_set:
+#       logging.debug("processing event")
+#     else:
+#       logging.debug("doing other work")
+      
+# e = threading.Event()
+# t2 = threading.Thread(name="nonblock",
+# target=wait_for_event_timeout,args=(e, 2))
+# t2.start()
+# logging.debug("Waiting before calling Event.set()")
+# time.sleep(7)
+# e.set()
+# logging.debug("Event is set")
+
+
+# exitapp = False
+# if __name__ == '__main__':
+#     try:
+#         main()
+#     except KeyboardInterrupt:
+#         exitapp = True
+#         raise
+
+# def threadCode(...):
+#     while not exitapp:
+#       pass

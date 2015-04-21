19e830415f3ec8ce00c8952bbe55fb87554e75a5
fzhurd <minganghz@gmail.com>
2015-04-20 21:20:02 -0700

    correct the error

--- PythonWork/thread/test_signal3c.py
+++ PythonWork/thread/test_signal3c.py
@@ -1,121 +1,88 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import threading
+import signal
 import time
-import logging
+import threading
+import os
 
-final_results=[]
+i=0
+def signal_handler(num, stack):
+    print time.ctime(), 'Alarm in', threading.currentThread()
 
-lock = threading.Lock()
-con = threading.Condition()
+def myHandler2(signum, frame):
+    print("Now, it's the time")
+    print os.getpid()
+    exit()
 
-def main():
-  keep_run_thread = threading.Thread(target=keep_run, name='alarm_thread') 
-  keep_run_thread.setDaemon(False)
-  keep_run_thread.start()
-  time.sleep(3)
-  # keep_run_thread.join(3)
-  print final_results
-  # time.sleep(4)
-  # if lock.acquire():
 
-    # print final_results
-    # con.notify()
-    # con.wait()
-    # lock.release()
+def use_alarm():
+  # while i>=5:
+  #   signal.alarm(1)
+    global i
+    while 1:
+      time.sleep(1)
+      # print time.ctime(), 'Setting alarm in', threading.currentThread()
+      print 'now monitor i is: ', i
+      if i>8:
+        print 'kill it due to i is above threshold'
+        signal.alarm(1)
+    #   # signal.alarm(10)
+    #   print time.ctime(), 'Sleeping in', threading.currentThread()
+    #   # time.sleep(3)
+    #   print time.ctime(), 'Done with sleep'
+    #   time.sleep(1)
 
 
 def keep_run():
-    i=0
-  # if lock.acquire():
-  # if con.acquire():
-    while i<10:
-      print i
-      i=i+1
-      time.sleep(1)
-      
-      final_results.append(i)
-      # con.notify()
-      # con.wait()
-    # lock.release()
 
+  global i
+  while 1:
+    print i
+    i=i+1
+    time.sleep(1)
 
+def main():
+  print os.getpid()
+  alarm_thread = threading.Thread(target=use_alarm, name='alarm_thread') 
+  alarm_thread.setDaemon(True)
+  alarm_thread.start()
+  # time.sleep(0.1)
+  
+  keep_run()
+  signal.signal(signal.SIGALRM, signal_handler)
+  
 
 if __name__=='__main__':
   main()
- 
-# num = 0
-# lock = threading.Lock()
- 
-# def func(st):
-#     global num
-#     print (threading.currentThread().getName() + ' try to acquire the lock')
-#     if lock.acquire():
-#         print (threading.currentThread().getName() + ' acquire the lock.' )
-#         print (threading.currentThread().getName() +" :%s" % str(num) )
-#         num += 1
-#         time.sleep(st)
-#         print (threading.currentThread().getName() + ' release the lock.'  )        
-#         lock.release()
- 
-# t1 = threading.Thread(target=func, args=(8,))
-# t2 = threading.Thread(target=func, args=(4,))
-# t3 = threading.Thread(target=func, args=(2,))
-# t1.start()
-# t2.start()
-# t3.start()
+
+
+# def signal_handler(num, stack):
+#     print time.ctime(), 'Alarm in', threading.currentThread()
+
+
+# signal.signal(signal.SIGALRM, signal_handler)
 
 
 
-# semaphore = threading.Semaphore(2)
- 
-# def func():
-#     if semaphore.acquire():
-#         for i in range(5):
-#           print (threading.currentThread().getName() + ' get semaphore')
-#         semaphore.release()
-#         print (threading.currentThread().getName() + ' release semaphore')
-        
-        
-# for i in range(4):
-#   t1 = threading.Thread(target=func)
-#   t1.start()
+# def use_alarm():
+#     print time.ctime(), 'Setting alarm in', threading.currentThread()
+#     signal.alarm(1)
+#     print time.ctime(), 'Sleeping in', threading.currentThread()
+#     time.sleep(3)
+#     print time.ctime(), 'Done with sleep'
 
 
-# logging.basicConfig(level=logging.DEBUG,
-# format="(%(threadName)-10s : %(message)s",
-# )
 
-# def wait_for_event_timeout(e, t):
-#     """Wait t seconds and then timeout"""
-#     while not e.isSet():
-#       logging.debug("wait_for_event_timeout starting")
-#       event_is_set = e.wait(t)
-#       logging.debug("event set: %s" % event_is_set)
-#     if event_is_set:
-#       logging.debug("processing event")
-#     else:
-#       logging.debug("doing other work")
-      
-# e = threading.Event()
-# t2 = threading.Thread(name="nonblock",
-# target=wait_for_event_timeout,args=(e, 2))
-# t2.start()
-# logging.debug("Waiting before calling Event.set()")
-# time.sleep(7)
-# e.set()
-# logging.debug("Event is set")
+# alarm_thread = threading.Thread(target=use_alarm, name='alarm_thread') 
+# alarm_thread.start()
+# time.sleep(0.1)
 
 
-# exitapp = False
-# if __name__ == '__main__':
-#     try:
-#         main()
-#     except KeyboardInterrupt:
-#         exitapp = True
-#         raise
 
-# def threadCode(...):
-#     while not exitapp:
-#       pass
+# print time.ctime(), 'Waiting for', alarm_thread
+# alarm_thread.join()
+
+
+
+# print time.ctime(), 'Exiting normally'

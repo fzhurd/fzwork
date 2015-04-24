98787ca152630718848282f8068bda98aef66107
fzhurd <minganghz@gmail.com>
2015-04-23 21:23:39 -0700

    more codes

--- JSWork/exercise/test1.js
+++ JSWork/exercise/test1.js
@@ -2,4 +2,9 @@ var x = myFunction(4, 3);
 
 function myFunction(a, b) {
     return a * b; 
-    }               
+    }
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+document.getElementById("demo").innerHTML = toCelsius(32);               

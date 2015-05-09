4225f82aa26ebe9f70a8905e2ed4b17e793c2947
fzhurd <minganghz@gmail.com>
2015-05-08 22:46:18 -0700

    meta

---
+++ PythonWork/test_meta_class3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class MyMeta(type):
+    def __new__(cls, name, bases, dct):
+        print "meta: creating %s %s" % (name, bases)
+        return type.__new__(cls, name, bases, dct)
+
+    def meta_meth(cls):
+        print "MyMeta.meta_meth"
+
+    __repr__ = lambda c: c.__name__
+
+class A(object):
+    __metaclass__ = MyMeta
+    def __init__(self):
+        super(A, self).__init__()
+        print "A init"
+
+    def meth(self):
+        print "A.meth"
+
+class B(object):
+    __metaclass__ = MyMeta
+    def __init__(self):
+        super(B, self).__init__()
+        print "B init"
+
+    def meth(self):
+        print "B.meth"
+
+class C(A, B):
+    __metaclass__ = MyMeta
+    def __init__(self):
+        super(C, self).__init__()
+        print "C init"

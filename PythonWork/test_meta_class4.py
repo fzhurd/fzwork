0de8eac8fbf1bc11b1c4376a64be828f63a90dce
fzhurd <minganghz@gmail.com>
2015-05-13 20:44:41 -0700

    more examples

--- PythonWork/test_meta_class4.py
+++ PythonWork/test_meta_class4.py
@@ -81,6 +81,39 @@ u = User(id=12345, name='Michael', email='test@orm.org', password='my-pwd')
 u.save()
 
 
+def ma(cls):  
+    print 'method a'  
+  
+def mb(cls):  
+    print 'method b'  
+  
+method_dict = {  
+    'ma': ma,  
+    'mb': mb,  
+}  
+  
+class DynamicMethod(type):  
+    def __new__(cls, name, bases, dct):  
+        if name[:3] == 'Abc':  
+            dct.update(method_dict)  
+        return type.__new__(cls, name, bases, dct)  
+  
+    def __init__(cls, name, bases, dct):  
+        super(DynamicMethod, cls).__init__(name, bases, dct)  
+  
+  
+class AbcTest(object):  
+    __metaclass__ = DynamicMethod  
+    def mc(self, x):  
+        print x * 3  
+  
+class NotAbc(object):  
+    __metaclass__ = DynamicMethod  
+    def md(self, x):  
+        print x * 3  
+  
+
+
 def main():
 
     pass

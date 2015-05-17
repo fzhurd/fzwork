d1988f0faf89d73310ade9c4608fb302476f29b7
fzhurd <minganghz@gmail.com>
2015-05-17 13:05:43 -0700

    added

--- PythonWork/test_meta_class5.py
+++ PythonWork/test_meta_class5.py
@@ -61,7 +61,28 @@ class Operation(object):
         print 'deleted %s' % str(x)  
   
   
+
+
+def monkey_patch(name, bases, dct):  
+    assert len(bases) == 1  
+    base = bases[0]  
+    for name, value in dct.iteritems():  
+        if name not in ('__module__', '__metaclass__'):  
+            setattr(base, name, value)  
+    return base  
   
+class A(object):  
+    def a(self):  
+        print 'i am A object'  
+  
+  
+class PatchA(A):  
+    __metaclass__ = monkey_patch  
+  
+    def patcha_method(self):  
+        print 'this is a method patched for class A'  
+  
+ 
 
 
 def main():
@@ -79,6 +100,13 @@ def main():
 
     op = Operation()  
     op.delete('test') 
+
+    print '&&&&&&&&&&&&&&&&&&&&&&&&&&777'
+    pa = PatchA()  
+    pa.patcha_method()  
+    pa.a()  
+    print dir(pa)  
+    print dir(PatchA)  
 
 if __name__=='__main__':
     main()

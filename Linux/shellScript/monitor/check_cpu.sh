91c7da768c347fcc2dfcabc5cf9f45e062bd7f81
fzhurd <minganghz@gmail.com>
2015-05-26 21:15:04 -0700

    add more scripts

--- Linux/shellScript/monitor/check_cpu.sh
+++ Linux/shellScript/monitor/check_cpu.sh
@@ -88,3 +88,22 @@ else
  echo "The number of des is normal"
  }
 fi
+
+function Listening
+ {
+ TCPListeningnum=`netstat -an | grep ":$1 " | \n
+ awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l`
+ UDPListeningnum=`netstat -an|grep ":$1 " \n
+ |awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l`
+ (( Listeningnum = TCPListeningnum + UDPListeningnum ))
+ if [ $Listeningnum == 0 ]
+ then
+ {
+ echo "0"
+ }
+ else
+ {
+ echo "1"
+ }
+ fi
+ }

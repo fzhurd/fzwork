85d967202a0c0a8a824e46f2c0fbf6cebf40b50c
fzhurd <minganghz@gmail.com>
2015-05-29 21:52:55 -0700

    check tcp

--- Linux/shellScript/monitor/check_tcp.sh
+++ Linux/shellScript/monitor/check_tcp.sh
@@ -1,3 +1,12 @@
 #!/bin/bash  
 tcp: netstat -an|egrep $1 |awk '$6 == "LISTEN" && $1 == "tcp" {print $0}'
 udp: netstat -an|egrep $1 |awk '$1 == "udp" && $5 == "0.0.0.0:*" {print $0}'
+
+
+function GetSysCPU
+{
+ CpuIdle=`vmstat 1 5 |sed -n '3,$p' \n
+ |awk '{x = x + $15} END {print x/5}' |awk -F. '{print $1}'
+ CpuNum=`echo "100-$CpuIdle" | bc`
+ echo $CpuNum
+}

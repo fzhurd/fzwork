aa9d882e848b3d96593268edf9eebc975dc980bf
fzhurd <minganghz@gmail.com>
2015-06-03 22:49:26 -0700

    more example

--- PythonWork/test_pm/test_index.py
+++ PythonWork/test_pm/test_index.py
@@ -4,9 +4,7 @@
 import os
 from pymongo import MongoClient
 import unittest
-
-import datetime
-import time
+import pymongo
 
 class Test_Index_1(unittest.TestCase):
 
@@ -28,7 +26,17 @@ class Test_Index_1(unittest.TestCase):
         self.db.col1.insert(col1)
         self.db.col1.ensure_index([('color', pymongo.ASCENDING)])
 
-        pipe_line =[{'$text': {'$search': "red"}}]
+
+    #     mongo.db.products.find(
+    # { '$text': { '$search': string } },
+    # fields=({ 'name': 1, 'foo': 1, 'bar': 1, 'score': { '$meta': 'textScore' } )
+
+
+        # self.db.command("text", "col1", search="red", project={"color": 1, "_id": 0}, limit=10)
+
+        # db.command('text', 'collection', search='coffee', filter={'about': {'$regex': 'desserts'}}, limit=2, projection={'comments': 1, '_id': 0}) 
+
+        pipe_line ={'$text': {'$search': "red"}}
         run_res = self.db.col1.find(pipe_line).count()
         print run_res, 'rrrrrrrrrrrrrrrrr'
         

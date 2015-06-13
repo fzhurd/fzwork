fe8f39d0284ee5826d3dac1f9679ce3b2138fd24
fzhurd <minganghz@gmail.com>
2015-06-13 16:10:04 -0700

    add

--- PythonWork/test_mongo_mysql/transfer_objId.py
+++ PythonWork/test_mongo_mysql/transfer_objId.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+
+
 def insert_into_mongo(input_csv_file):
     file_path = '/home/frank/workAtHome/workLog/'
-    input_file = file_path+input_csv_file
+    input_file = os.path.join(file_path, input_csv_file)
     with open(input_file,'r+') as f:
-        print f
+        for i in f:
+            print i
+            element = i.split(',')
+            print type(element), element[0]
 
 def main():
-    insert_into_mongo('col2mysql.csdv')
+    # print os.getcwd()
+    insert_into_mongo('col2mysql.csv')
     
 
 

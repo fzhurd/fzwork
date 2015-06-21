060059587e3e3f1b911ff879e374d5e6e20c98cb
fzhurd <minganghz@gmail.com>
2015-06-20 21:36:34 -0700

    add plugin

--- PythonWork/noseplugin/customer_parameters/test_customer_arguments.py
+++ PythonWork/noseplugin/customer_parameters/test_customer_arguments.py
@@ -10,55 +10,80 @@ from time import time
 from nose.plugins.base import Plugin
 
  
-class TestTimer(Plugin):
+class Customer_Parameters(Plugin):
   
-    name = 'test-timer'
+    name = 'customer_parameters_plugin'
     score = 1
     enabled = True
+
+
+    def add_options(self, parser, env=os.environ):
+       '''Add command-line options for plugin'''
+        env_opt = 'NOSE_PASTE_SETUP_FIRST'
+        
+        parser.add_option('--paste-setup-first',
+                          action='store_true',
+                          default=env.get('env_opt'),
+                         dest='paste_setup',
+                         help='Run setup-app in Paste before running tests.  [%s]' % env_opt)
+        
+    def configure(self, options, conf):
+        """Configure the plugin"""
+        Plugin.configure(self, options, conf)
+        if options.paste_setup:
+            self.enabled = True
+        
+    def begin(self):
+        '''Called before any tests are collected or run.  Resets database.'''
+        from paste.script.appinstall import SetupCommand
+
+        # Select the .ini file to run setup-app on
+        test_file = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'test.ini')
+        SetupCommand('setup-app').run([test_file])
  
-    def _timeTaken(self):
-        if hasattr(self, '_timer'):
-            taken = time() - self._timer
-        else:
-            # test died before it ran (probably error in setup())
-            # or success/failure added before test started probably
-            # due to custom TestResult munging
-            taken = 0.0
-        return taken
+    # def _timeTaken(self):
+    #     if hasattr(self, '_timer'):
+    #         taken = time() - self._timer
+    #     else:
+    #         # test died before it ran (probably error in setup())
+    #         # or success/failure added before test started probably
+    #         # due to custom TestResult munging
+    #         taken = 0.0
+    #     return taken
  
-    def options(self, parser, env):
-        """Sets additional command line options."""
-        super(TestTimer, self).options(parser, env)
+    # def options(self, parser, env):
+    #     """Sets additional command line options."""
+    #     super(TestTimer, self).options(parser, env)
  
-    def configure(self, options, config):
-        """Configures the test timer plugin."""
-        super(TestTimer, self).configure(options, config)
-        self.config = config
-        self._timed_tests = {}
+    # def configure(self, options, config):
+    #     """Configures the test timer plugin."""
+    #     super(TestTimer, self).configure(options, config)
+    #     self.config = config
+    #     self._timed_tests = {}
  
-    def startTest(self, test):
-        """Initializes a timer before starting a test."""
-        self._timer = time()
+    # def startTest(self, test):
+    #     """Initializes a timer before starting a test."""
+    #     self._timer = time()
  
-    def report(self, stream):
-        """Report the test times"""
-        if not self.enabled:
-            return
-        d = sorted(self._timed_tests.iteritems(), key=operator.itemgetter(1))
-        for test, time_taken in d:
-            stream.writeln("%s: %0.4f" % (test, time_taken))
+    # def report(self, stream):
+    #     """Report the test times"""
+    #     if not self.enabled:
+    #         return
+    #     d = sorted(self._timed_tests.iteritems(), key=operator.itemgetter(1))
+    #     for test, time_taken in d:
+    #         stream.writeln("%s: %0.4f" % (test, time_taken))
  
-    def _register_time(self, test):
-        self._timed_tests[test.id()] = self._timeTaken()
+    # def _register_time(self, test):
+    #     self._timed_tests[test.id()] = self._timeTaken()
  
-    def addError(self, test, err, capt=None):
-        self._register_time(test)
+    # def addError(self, test, err, capt=None):
+    #     self._register_time(test)
  
-    def addFailure(self, test, err, capt=None, tb_info=None):
-        self._register_time(test)
+    # def addFailure(self, test, err, capt=None, tb_info=None):
+    #     self._register_time(test)
  
-    def addSuccess(self, test, capt=None):
-        self._register_time(test)
+    # def addSuccess(self, test, capt=None):
+    #     self._register_time(test)
  
  
 if __name__ == '__main__':
@@ -83,13 +108,3 @@ if __name__ == '__main__':
 #     def finalize(self, result):
 #         log.info('Hello pluginized world!')
 
-
-# def main():
-#     print 'hi'
-#     pass
-
-
-
-
-# if __name__=='__main__':
-#     main()

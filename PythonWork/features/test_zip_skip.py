12b1d270200460102d70d1da5926317204c25a77
fzhurd <minganghz@gmail.com>
2015-07-12 11:08:05 -0700

    add izip

--- PythonWork/features/test_zip_skip.py
+++ PythonWork/features/test_zip_skip.py
@@ -1,8 +1,19 @@
 # usr/bin/python
 # -*- coding: utf-8 -*-
+import itertools
 
 def main():
-	pass
+    test1()
+	
+def test_1:
+    a = [1,2,3,4,5,6,19,20]
+    b = [7,8,9,10,11,17,18]
+    it_a = iter(a)
+    it_b = iter(b)
+    for x,y in itertools.izip(it_a, it_b):
+        if x == 5:
+            x = next(it_a)
+            print x,y
 
 
 

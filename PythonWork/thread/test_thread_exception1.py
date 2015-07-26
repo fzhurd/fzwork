138173a9d188f6bd087494321bf183f83384baa7
fzhurd <minganghz@gmail.com>
2015-07-25 21:06:42 -0700

    changes

--- PythonWork/thread/test_thread_exception1.py
+++ PythonWork/thread/test_thread_exception1.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import itertools
-import pymysql
-
-def main():
-
-    set_up_pymysql()
-    
-    # test1()
-
-def test1():
-    
-    a = [1,2,3,4,5,6,19,20]
-    b = [7,8,9,10,11,17,18]
-    it_a = iter(a)
-    it_b = iter(b)
-    for x,y in itertools.izip(it_a, it_b):
-        if x == 5:
-            x = next(it_a)
-        print x,y
-
-def set_up_pymysql():
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='root', db='test')
-    cur = conn.cursor()
-    cur.execute("SELECT * from col6")
-
-    print(cur.description)
-
-    print()
-
-    for row in cur:
-       print(row)
-
-    cur.close()
-    conn.close()
+import unittest
 
 
 
-if __name__ == '__main__':
-    main()
+
+class Regression_Tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def run_concurrent():
+        for i in xrange(100):
+            print i
+            if i==50:
+
+
+
+    def test_cocurrent_running():
+
+        threads =[]
+
+
+        t1 = threading.Thread(target=run_concurrent)
+        t2 = threading.Thread(target=run_concurrent)
+       
+
+        threads.append(t1)
+        threads.append(t2)
+        
+        t1.start()
+        t2.start()
+   
+
+
+
+    	   
+
+

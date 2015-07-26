bdd30f871f6c86c6b0e8797b4f1a05b54adc20bd
fzhurd <minganghz@gmail.com>
2015-07-26 00:05:09 -0700

    threading tests first commit

--- PythonWork/thread/test_thread_exception1.py
+++ PythonWork/thread/test_thread_exception1.py
@@ -4,9 +4,12 @@
 import unittest
 import threading
 import time
+import sys
+import signal
 
 
-
+is_exception = False
+exc_info = None
 
 class Regression_Tests(unittest.TestCase):
 
@@ -14,28 +17,48 @@ class Regression_Tests(unittest.TestCase):
         pass
 
     def run_concurrent(self):
+        global exc_info
         for i in xrange(10):
             time.sleep(1)
             print i
             if i==5:
-                print 'exception'
+                try:
+                    assert i==6
+                except Exception as e:
+                    signal.alarm(1)
+                    
+                    exc_info=sys.exc_info()
+                    print exc_info, 'eeeeeeeeeeeee'
+                    is_exception=True
+                    self.fail(e)
+                
+                # self.assertEqual(i,6)
+                # self.assertRaises(SomeException) as e:
+
 
 
     def test_cocurrent_running(self):
+        global exe_info
 
-        threads =[]
+        # threads =[]
 
+        try:
+            t1 = threading.Thread(target=self.run_concurrent)
 
-        t1 = threading.Thread(target=self.run_concurrent)
-        # t2 = threading.Thread(target=self.run_concurrent)
-       
+            t1.start()
 
-        threads.append(t1)
-        # threads.append(t2)
+            t1.join()
+
+            # print type(exc_info), exc_info
+
+            # time.sleep(2)
+
+            if exc_info:
+                raise AssertionError('threading failed')
+   
+        except AssertionError as e:
+            self.fail(e)
         
-        t1.start()
-        # t2.start()
-        t1.join()
    
 
 

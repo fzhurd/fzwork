a15e97a8e3de1fba806058e1461bfa91f52b900d
fzhurd <minganghz@gmail.com>
2015-07-26 21:29:46 -0700

    added

--- PythonWork/test_change_dir.py
+++ PythonWork/test_change_dir.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+
 def main():
-	print 'hi'
+	home = os.environ.get('TMPTESTHOME')
+	print home
+
+	print os.getcwd()
+	os.chdir("/home/frank/workAtHome")
+
+	print os.getcwd()
+	current_dir=os.getcwd()
+
+	all_folders = os.listdir(current_dir)
+	print all_folders
+
+	os.rename("/home/frank/workAtHome/tmp_test","/home/frank/workAtHome/tmp_test_after_change")
+
+	all_folders_after_change=os.listdir(current_dir)
+	print all_folders_after_change
+
+	print '***********************************'
+
+	os.chdir(home)
+
+	print os.getcwd()
+	
 
 
 if __name__ == '__main__':

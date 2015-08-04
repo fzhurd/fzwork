449848f0d2ed73344662a6174a5b34ec9308ede0
fzhurd <minganghz@gmail.com>
2015-08-03 20:34:32 -0700

    inital commit

---
+++ JavaWork/exercise/junit1/TestJunit.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class TestJunit {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
+}

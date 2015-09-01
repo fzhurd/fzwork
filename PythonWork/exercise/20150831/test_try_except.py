12a6c689a6cc08c4c9ff49f56496652bcb7f10aa
fzhurd <minganghz@gmail.com>
2015-08-31 23:10:59 -0700

    done

--- PythonWork/exercise/20150831/test_try_except.py
+++ PythonWork/exercise/20150831/test_try_except.py
@@ -14,20 +14,24 @@ from functools import wraps
 #             print "Oh noes"
 #     return handleProblems
 
-def handleError(self, func):
-    @wraps(func)
-    def wrapper( *args, **kwargs):
-        try:  
-            func(*args, **kwargs)
-        except Exception as e:
-            self.fail(e)
 
-            # print "oh, noes"
-    return wrapper
 
 
 
 class Test_Simple_Query(unittest.TestCase):
+
+
+    def handleError(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            try:  
+                func(self, *args, **kwargs)
+            except Exception as e:
+                # raise Exception("Boom!")
+                self.fail(e)
+
+                # print "oh, noes"
+        return wrapper
 
 
     def setUp(self):
@@ -44,21 +48,21 @@ class Test_Simple_Query(unittest.TestCase):
 
             return conn
 
-    @handleError
-    def test_simple_TC_1(self):
+    # @handleError
+    # def test_simple_TC_1(self):
 
-            self.query = 'select a from t1;'
-            self.cursor.execute(self.query)
+    #         self.query = 'select a from t1;'
+    #         self.cursor.execute(self.query)
 
-            results = [doc for doc in self.cursor]
-            print results
+    #         results = [doc for doc in self.cursor]
+    #         print results
 
-            expected_results=[('red',), ('green',)]
-            self.assertItemsEqual(results, expected_results)
+    #         expected_results=[('red',), ('green',)]
+    #         self.assertItemsEqual(results, expected_results)
 
     @handleError
     def test_simple_TC_2(self):
             self.assertEqual(1, 2)
-            raise Exception("Boom!")
+            # raise Exception("Boom!")
 
 

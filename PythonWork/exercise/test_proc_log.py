af5f68c5e86d141fb32aa1e80b3dc0c3d79a775e
fzhurd <minganghz@gmail.com>
2015-10-08 22:33:22 -0700

    tail

--- PythonWork/exercise/test_proc_log.py
+++ PythonWork/exercise/test_proc_log.py
@@ -4,10 +4,16 @@ import psutil
 
 
 def main():
-	# print 'hi'
+	find_process()
+
+def find_process():
 	for proc in psutil.process_iter():
 		if proc.name()=='sonarsql':
 			print proc.cmdline()
+
+
+def tail():
+	pass
 
 
 if __name__=='__main__':

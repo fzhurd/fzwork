47bce45fb63a3a4504a8044fa3119b40e64914a8
fzhurd <minganghz@gmail.com>
2015-10-08 21:27:22 -0700

    test

--- PythonWork/exercise/test_proc_log.py
+++ PythonWork/exercise/test_proc_log.py
@@ -6,8 +6,8 @@ import psutil
 def main():
 	# print 'hi'
 	for proc in psutil.process_iter():
-		if proc.name()=='mongod':
-			print proc
+		if proc.name()=='sonarsql':
+			print proc.cmdline()
 
 
 if __name__=='__main__':

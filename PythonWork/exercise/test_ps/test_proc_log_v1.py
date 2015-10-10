94bd0c34a6537020211351c2ca2a5f7683e73c57
fzhurd <minganghz@gmail.com>
2015-10-09 21:27:09 -0700

    initial commit

---
+++ PythonWork/exercise/test_ps/test_proc_log_v1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pymysql
+import unittest
+import psutil
+
+HOST='127.0.0.1'
+USER='test'
+PASSWD='test'
+DATABASE='test'
+PORT=3307
+
+class Test_Proc_Log(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def find_process(self, process_name):
+        for proc in psutil.process_iter():
+            if proc.name()==process_name:
+                process_cmdline = proc.cmdline()
+                # print proc.cmdline()
+                return process_cmdline
+
+
+    def test_proc(self):
+        proc_cmdline = self.find_process('mongod')
+        print proc_cmdline

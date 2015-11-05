29e1e1609b3be459ba32ab61ffd008554be3859e
fzhurd <minganghz@gmail.com>
2015-11-04 22:59:41 -0800

    subprocess

--- PythonWork/exercise/test_subprocess.py
+++ PythonWork/exercise/test_subprocess.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import subprocess
+from subprocess import Popen, PIPE
+
+
 
 def main():
-    print 'test'
+
+    res = process = Popen(['ls'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    print res
 
 if __name__ == '__main__':
     main()

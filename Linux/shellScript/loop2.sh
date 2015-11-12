74d333ea4f05d205b055ca0d9ec5dd60359fb40d
fzhurd <minganghz@gmail.com>
2015-11-11 16:46:53 -0800

    add more examples

--- Linux/shellScript/loop2.sh
+++ Linux/shellScript/loop2.sh
@@ -1,15 +1,9 @@
 #!/bin/bash 
 
-# echo {1..10}
-for i in {1..20}
+echo {1..5}
+
+for i in {1..10}
 do
 	echo $i
 done
 
-
-# echo "enter a file name:"  
-# read a  
-# if test  -e /root/$a   
-# then echo "the file is exist!"  
-# else echo "the file is not exist!"  
-# fi  

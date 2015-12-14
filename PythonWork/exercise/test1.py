54f0a07d40ed9f1fcdd6a19d9de73098d097b2ee
fzhurd <minganghz@gmail.com>
2015-12-13 19:27:58 -0800

    added

---
+++ PythonWork/exercise/test1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import os
+class Student(object):
+    def __init__(self, studentId, name):
+        pass
+
+def generator():
+    for x in xrange(4):
+        yield x+1
+        print('incrementing x')
+
+def main():
+    print 'hello, first example'
+
+    result = generator()
+    print result
+    print type(result)
+    # for i in result:
+    # 	print i
+    print result.next()
+    print result.next()
+    print result.next()
+    print result.next()
+    print result.next()
+    print result.next()
+	
+if __name__ == '__main__':
+    main()

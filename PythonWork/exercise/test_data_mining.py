185b0b682544371c6ac98ba38ccbbebc6a4e276a
fzhurd <minganghz@gmail.com>
2015-12-23 21:04:43 -0800

    first part

---
+++ PythonWork/exercise/test_data_mining.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+import os
+
+import urllib2
+url = 'http://aima.cs.berkeley.edu/data/iris.csv'
+u = urllib2.urlopen(url)
+localFile = open('iris.csv', 'w')
+localFile.write(u.read())
+localFile.close()

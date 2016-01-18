3aa6ab27790b54aab8210d360cba88ea7ce1ff82
fzhurd <minganghz@gmail.com>
2016-01-17 19:43:39 -0800

    function 5

--- Linux/shellScript/function5.sh
+++ Linux/shellScript/function5.sh
@@ -7,3 +7,27 @@ fSum 3 2;
  fSum 5 7;
  total=$(fSum 3 2);
  echo $total,$?;
+
+
+ echo $(uname);
+declare num=1000;
+ 
+uname()
+{
+    echo "test!";
+    ((num++));
+    return 100;
+}
+testvar()
+{
+    local num=10;
+    ((num++));
+    echo $num;
+ 
+}
+ 
+uname;
+echo $?
+echo $num;
+testvar;
+echo $num;

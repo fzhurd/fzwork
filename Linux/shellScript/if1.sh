71010bd6d750ee326bda6da34270eea9c7b4d4f3
fzhurd <minganghz@gmail.com>
2016-01-26 20:42:58 -0800

    first examples

--- Linux/shellScript/if1.sh
+++ Linux/shellScript/if1.sh
@@ -1,3 +1,15 @@
 #!/bin/bash 
  
 a=5;if [[ a -gt 4 ]] ;then echo 'ok';fi;
+
+
+scores=40;
+if [[ $scores -gt 90 ]]; then
+ echo "very good!";
+elif [[ $scores -gt 80 ]]; then
+ echo "good!";
+elif [[ $scores -gt 60 ]]; then
+ echo "pass!";
+else
+ echo "no pass!";
+fi;

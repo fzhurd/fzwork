eb731ed9cf7f5a48d06ab83bbc98844676aad771
fzhurd <minganghz@gmail.com>
2016-02-16 21:00:32 -0800

    add2

--- PythonWork/exercise/decorator1.py
+++ PythonWork/exercise/decorator1.py
@@ -23,3 +23,14 @@ class Foo(object):
     def name(self):
         return self._name
 
+class DBManage(object):
+    @classmethod
+    def table_name(cls):
+        return cls.__name__.lower()
+ 
+    @classmethod
+    def select_all(cls):
+        sql = "SELECT * FROM %s""" % cls.table_name()
+        # 执行这个语句的代码
+        return result
+

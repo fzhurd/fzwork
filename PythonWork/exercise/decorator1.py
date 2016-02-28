57bd098722c92fe2056a8aab3e85d06cc95ed374
fzhurd <minganghz@gmail.com>
2016-02-28 14:10:06 -0800

    added

--- PythonWork/exercise/decorator1.py
+++ PythonWork/exercise/decorator1.py
@@ -150,4 +150,38 @@ myfunc()
 myfunc8()
 
 print '\n'
-print '*************************step 8********************'
+print '*************************step 9********************'
+
+class mylocker:
+    def __init__(self):
+        print("mylocker.__init__() called.")
+         
+    @staticmethod
+    def acquire():
+        print("mylocker.acquire() called.")
+         
+    @staticmethod
+    def unlock():
+        print("  mylocker.unlock() called.")
+
+class lockerex(mylocker):
+    @staticmethod
+    def acquire():
+        print("lockerex.acquire() called.")
+         
+    @staticmethod
+    def unlock():
+        print("  lockerex.unlock() called.")
+
+def lockhelper(cls):
+    '''cls 必须实现acquire和release静态方法'''
+    def _deco(func):
+        def __deco(*args, **kwargs):
+            print("before %s called." % func.__name__)
+            cls.acquire()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                cls.unlock()
+        return __deco
+    return _deco

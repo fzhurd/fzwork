271607b42bf3ba9bf27e3db8ccca1473a5008f48
fzhurd <minganghz@gmail.com>
2016-02-28 15:30:40 -0800

    add more codes

--- PythonWork/datascience/data_mining_v1.py
+++ PythonWork/datascience/data_mining_v1.py
@@ -18,3 +18,16 @@ dataset = np.loadtxt(raw_data, delimiter=",")
 # separate the data from the target attributes
 X = dataset[:,0:7]
 y = dataset[:,8]
+
+from sklearn import preprocessing
+# normalize the data attributes
+normalized_X = preprocessing.normalize(X)
+# standardize the data attributes
+standardized_X = preprocessing.scale(X)
+
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
+model = ExtraTreesClassifier()
+model.fit(X, y)
+# display the relative importance of each attribute
+print(model.feature_importances_)

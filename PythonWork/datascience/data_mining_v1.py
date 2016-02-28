df1e640f66ef904ca55f866d2f5e8b3a9714879c
fzhurd <minganghz@gmail.com>
2016-02-28 15:27:55 -0800

    add skiklearn

--- PythonWork/datascience/data_mining_v1.py
+++ PythonWork/datascience/data_mining_v1.py
@@ -6,3 +6,15 @@ u = urllib2.urlopen(url)
 localFile = open('iris.csv', 'w')
 localFile.write(u.read())
 localFile.close()
+
+import numpy as np
+import urllib
+# url with dataset
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+# download the file
+raw_data = urllib.urlopen(url)
+# load the CSV file as a numpy matrix
+dataset = np.loadtxt(raw_data, delimiter=",")
+# separate the data from the target attributes
+X = dataset[:,0:7]
+y = dataset[:,8]

ac04daff3b060708eb5c5853ff91fbf054f8bbe9
fzhurd <minganghz@gmail.com>
2016-02-28 14:37:42 -0800

    add the class

--- PythonWork/exercise/decorator1.py
+++ PythonWork/exercise/decorator1.py
@@ -187,3 +187,23 @@ def lockhelper(cls):
     return _deco
 
 
+from mylocker import *
+ 
+class example:
+    @lockhelper(mylocker)
+    def myfunc(self):
+        print(" myfunc() called.")
+ 
+    @lockhelper(mylocker)
+    @lockhelper(lockerex)
+    def myfunc2(self, a, b):
+        print(" myfunc2() called.")
+        return a + b
+ 
+# if __name__=="__main__":
+#     a = example()
+#     a.myfunc()
+#     print(a.myfunc())
+#     print(a.myfunc2(1, 2))
+#     print(a.myfunc2(3, 4))
+

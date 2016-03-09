eb58f045cfe15ace7ed2107380b2c33ec4b96c52
fzhurd <minganghz@gmail.com>
2016-03-08 21:12:56 -0800

    add CART

--- PythonWork/datascience/data_mining_v1.py
+++ PythonWork/datascience/data_mining_v1.py
@@ -83,3 +83,17 @@ predicted = model.predict(X)
 # summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
+
+print '********************CART**************************'
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+# fit a CART model to the data
+model = DecisionTreeClassifier()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))

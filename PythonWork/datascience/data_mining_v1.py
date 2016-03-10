8d903eedbb3168ccd2bd294fd540a3d9e2b47471
fzhurd <minganghz@gmail.com>
2016-03-09 20:39:43 -0800

    add SVM

--- PythonWork/datascience/data_mining_v1.py
+++ PythonWork/datascience/data_mining_v1.py
@@ -97,3 +97,17 @@ predicted = model.predict(X)
 # summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
+
+print '********************SVM**************************'
+from sklearn import metrics
+from sklearn.svm import SVC
+# fit a SVM model to the data
+model = SVC()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))

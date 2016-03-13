00fccf1fb80d39a5c0bc5b685cfefb7ee0b72d86
fzhurd <minganghz@gmail.com>
2016-03-12 21:11:33 -0800

    add

--- PythonWork/datascience/data_mining_v1.py
+++ PythonWork/datascience/data_mining_v1.py
@@ -111,3 +111,17 @@ predicted = model.predict(X)
 # summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
+
+import numpy as np
+from sklearn.linear_model import Ridge
+from sklearn.grid_search import GridSearchCV
+# prepare a range of alpha values to test
+alphas = np.array([1,0.1,0.01,0.001,0.0001,0])
+# create and fit a ridge regression model, testing each alpha
+model = Ridge()
+grid = GridSearchCV(estimator=model, param_grid=dict(alpha=alphas))
+grid.fit(X, y)
+print(grid)
+# summarize the results of the grid search
+print(grid.best_score_)
+print(grid.best_estimator_.alpha)

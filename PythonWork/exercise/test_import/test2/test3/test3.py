71149823668e426ca9347c9cf20acc47ae3fa848
fzhurd <minganghz@gmail.com>
2016-03-21 21:30:00 -0700

    add

--- PythonWork/exercise/test_import/test2/test3/test3.py
+++ PythonWork/exercise/test_import/test2/test3/test3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-import ..test2
+# from test_import.test2 import func2
+import test3b
 
 def func3():
     print 'this is function 3'
 
 
 func3()
-func2()
+test3b.func3b()
+# test2.func2()

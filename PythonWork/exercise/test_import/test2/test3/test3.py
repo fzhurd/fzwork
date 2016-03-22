2e71284c1b410eb70d0742e5fb0235efdf4ef8c3
fzhurd <minganghz@gmail.com>
2016-03-21 21:48:26 -0700

    add2

--- PythonWork/exercise/test_import/test2/test3/test3.py
+++ PythonWork/exercise/test_import/test2/test3/test3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # from test_import.test2 import func2
 import test3b
+from .. import test2
 
 def func3():
     print 'this is function 3'
@@ -8,4 +9,4 @@ def func3():
 
 func3()
 test3b.func3b()
-# test2.func2()
+test2.func2()

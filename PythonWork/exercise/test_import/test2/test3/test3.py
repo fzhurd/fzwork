6f8165b2d139640c733c9ce585e63588f679ba05
fzhurd <minganghz@gmail.com>
2016-03-27 21:18:52 -0700

    add

--- PythonWork/exercise/test_import/test2/test3/test3.py
+++ PythonWork/exercise/test_import/test2/test3/test3.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
-# from test_import.test2 import func2
+from test_import.test2 import func2
 import test3b
-from .. import test2
+from test2 import func2
 
 def func3():
     print 'this is function 3'
 
 
-func3()
-test3b.func3b()
-test2.func2()
+
+
+
+if __name__=='__main__':
+    func3()
+    test3b.func3b()
+    test2.func2()

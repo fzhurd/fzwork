7d7524931f8bbe75fd77fdf9af324e4fa472f3ee
fzhurd <minganghz@gmail.com>
2016-04-22 22:41:21 -0700

    work well examples

--- PythonWork/noseplugin/customer_parameters/temp2/config_plug.py
+++ PythonWork/noseplugin/customer_parameters/temp2/config_plug.py
@@ -2,11 +2,50 @@
 # -*- coding: utf-8 -*-
 
 from nose.plugins import Plugin
+import nose
+import unittest
+
+
+class ConfigurableWidget(object):
+    cfg = None
+    def can_frobnicate(self):
+        return self.cfg.get('can_frobnicate', True)
+
+    def likes_cheese(self):
+        return self.cfg.get('likes_cheese', True)
+
+class TestConfigurableWidget(unittest.TestCase):
+    longMessage = False
+    def setUp(self):
+        self.widget = ConfigurableWidget()
+
+    def test_can_frobnicate(self):
+        self.widget.can_frobnicate()
+
+    def test_likes_cheese(self):
+        self.widget.likes_cheese()
+
+    def shortDescription(self):
+        try:
+            doc = self._testMethodDoc
+        except AttributeError:
+            doc = self._TestCase__testMethodDoc
+            return doc and doc.split("\n")[0].strip() or None
 
 class ConfiguringPlugin(Plugin):
-	enabled = True
-	def configure(self, options, conf):
-		pass 
+    enabled = True
+    def configure(self, options, conf):
+        pass
 
-	def begin(self):
-		ConfigurableWidget.cfg = {}
+    def begin(self):
+        ConfigurableWidget.cfg = {}
+
+def suite():
+    return unittest.TestSuite([
+        TestConfigurableWidget('test_can_frobnicate'),
+        TestConfigurableWidget('test_likes_cheese')])
+
+if __name__ == '__main__':
+    argv = [__file__, '-v']
+    nose.run(argv=argv, suite=suite(),plugins=[ConfiguringPlugin()])  
+

e97787d677bb87b56881442a7c4f04686af44616
fzhurd <minganghz@gmail.com>
2016-05-17 21:17:17 -0700

    add q258

---
+++ PythonWork/codeexer/q258_add_digits.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Question 258: Add Digits
+
+Given a non-negative integer num, repeatedly add all its digits 
+until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. 
+Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime? 
+
+"""
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        num_str=str(num)
+        if len(num_str)==1:
+            return num
+        
+        while len(num_str)>1:
+            sum=0
+            for i in num_str:
+                sum=sum+int(i)
+            num_str=str(sum)
+            if len(str(sum))==1:
+                return sum
+            else:
+                self.addDigits(sum)

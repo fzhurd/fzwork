82bf7f7f7e23066029981ef1984f166ca08f26de
fzhurd <minganghz@gmail.com>
2016-05-30 22:26:22 -0700

    add q350

--- PythonWork/codeexer/q350_intersection_of_two_arrays_II.py
+++ PythonWork/codeexer/q350_intersection_of_two_arrays_II.py
@@ -49,10 +49,27 @@ class Solution(object):
         # return final
         
         #solution 2
-        c1 = collections.Counter(nums1)
-        c2 = collections.Counter(nums2)
-        result = []
-        for n in c1:
-            if n in c2:
-                result += [n] * min(c1[n], c2[n])
-        return result
+        # c1 = collections.Counter(nums1)
+        # c2 = collections.Counter(nums2)
+        # result = []
+        # for n in c1:
+        #     if n in c2:
+        #         result += [n] * min(c1[n], c2[n])
+        # return result
+        
+        #solution 3
+        nums1.sort()
+        nums2.sort()
+        res = []
+        a = 0
+        b = 0
+        while a < len(nums1) and b < len(nums2):
+            if nums1[a] == nums2[b]:
+                res.append(nums1[a])
+                a += 1
+                b += 1
+            elif nums1[a] < nums2[b]:
+                a += 1
+            else:
+                b += 1
+        return res

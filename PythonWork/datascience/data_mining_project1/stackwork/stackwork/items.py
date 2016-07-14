6a9c7956c0b66c7a73cede49497943d462659e54
fzhurd <minganghz@gmail.com>
2016-07-13 23:31:08 -0700

    add

--- PythonWork/datascience/data_mining_project1/stackwork/stackwork/items.py
+++ PythonWork/datascience/data_mining_project1/stackwork/stackwork/items.py
@@ -6,9 +6,11 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+# from scrapy.item import Item, Field
 
 
 class StackworkItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    url = scrapy.Field()

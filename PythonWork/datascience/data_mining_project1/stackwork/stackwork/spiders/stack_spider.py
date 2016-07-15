beddad5d6701df37e6c4cae1e71b0d20dd52087f
fzhurd <minganghz@gmail.com>
2016-07-14 23:11:59 -0700

    add2

--- PythonWork/datascience/data_mining_project1/stackwork/stackwork/spiders/stack_spider.py
+++ PythonWork/datascience/data_mining_project1/stackwork/stackwork/spiders/stack_spider.py
@@ -6,18 +6,22 @@ from scrapy.selector import Selector
  
  
 class StackSpider(Spider):
-    name = "stack"
+    name = "stack_spider"
     allowed_domains = ["stackoverflow.com"]
     start_urls = [
         "http://stackoverflow.com/questions?pagesize=50&sort=newest",
     ]
 
     def parse(self, response):
+
         questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
         for question in questions:
+
             item = StackItem()
-            item['title'] = question.xpath(
-                'a[@class="question-hyperlink"]/text()').extract()[0]
-            item['url'] = question.xpath(
-                'a[@class="question-hyperlink"]/@href').extract()[0]
+
+            item['title'] = question.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
+
+            item['url'] = question.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+            
             yield item

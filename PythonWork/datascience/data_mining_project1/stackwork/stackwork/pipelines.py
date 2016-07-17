2aa337acf10e7df5042b2e704ad0d0801a7323b0
fzhurd <minganghz@gmail.com>
2016-07-16 22:04:27 -0700

    add

--- PythonWork/datascience/data_mining_project1/stackwork/stackwork/pipelines.py
+++ PythonWork/datascience/data_mining_project1/stackwork/stackwork/pipelines.py
@@ -8,19 +8,33 @@
 import pymongo
  
 from scrapy.conf import settings
+
+from scrapy.exceptions import DropItem
+from scrapy import log
  
  
 class MongoDBPipeline(object):
  
     def __init__(self):
-        connection = pymongo.Connection(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT']
-        )
+
+        connection = pymongo.Connection(settings['MONGODB_SERVER'],settings['MONGODB_PORT'])
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
 
 
 class StackworkPipeline(object):
-    def process_item(self, item, spider):
+
+    # def process_item(self, item, spider):
+    #     return item
+
+     def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+            log.msg("Question added to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
         return item

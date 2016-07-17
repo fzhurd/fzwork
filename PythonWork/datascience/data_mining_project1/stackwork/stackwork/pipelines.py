3335449d055bd741993ef306ccdadd9a4688cc52
fzhurd <minganghz@gmail.com>
2016-07-16 20:31:50 -0700

    add

--- PythonWork/datascience/data_mining_project1/stackwork/stackwork/pipelines.py
+++ PythonWork/datascience/data_mining_project1/stackwork/stackwork/pipelines.py
@@ -5,6 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+ 
+from scrapy.conf import settings
+ 
+ 
+class MongoDBPipeline(object):
+ 
+    def __init__(self):
+        connection = pymongo.Connection(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
 
 class StackworkPipeline(object):
     def process_item(self, item, spider):

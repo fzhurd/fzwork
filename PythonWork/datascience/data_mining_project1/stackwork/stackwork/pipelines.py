41d94113d16216dea3ccf4f33642535aa30a8430
fzhurd <minganghz@gmail.com>
2016-07-17 21:47:22 -0700

    add comment

--- PythonWork/datascience/data_mining_project1/stackwork/stackwork/pipelines.py
+++ PythonWork/datascience/data_mining_project1/stackwork/stackwork/pipelines.py
@@ -20,6 +20,13 @@ class MongoDBPipeline(object):
         connection = pymongo.Connection(settings['MONGODB_SERVER'],settings['MONGODB_PORT'])
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
+        # self.server = settings['MONGODB_SERVER']
+        # self.port = settings['MONGODB_PORT']
+        # self.db = settings['MONGODB_DB']
+        # self.col = settings['MONGODB_COLLECTION']
+        # connection = pymongo.Connection(self.server, self.port)
+        # db = connection[self.db]
+        # self.collection = db[self.col]
 
 
 class StackworkPipeline(object):
@@ -27,14 +34,18 @@ class StackworkPipeline(object):
     # def process_item(self, item, spider):
     #     return item
 
-     def process_item(self, item, spider):
-        valid = True
-        for data in item:
-            if not data:
-                valid = False
-                raise DropItem("Missing {0}!".format(data))
-        if valid:
-            self.collection.insert(dict(item))
-            log.msg("Question added to MongoDB database!",
-                    level=log.DEBUG, spider=spider)
-        return item
+	def process_item(self, item, spider):
+	    valid = True
+	    for data in item:
+	        if not data:
+	            valid = False
+	            raise DropItem("Missing {0}!".format(data))
+	    if valid:
+	        self.collection.insert(dict(item))
+	        log.msg("Question added to MongoDB database!",
+	                level=log.DEBUG, spider=spider)
+	    return item
+
+    # self.collection.insert(dict(item))
+    # log.msg('Item written to MongoDB database %s/%s' % (self.db, self.col),level=log.DEBUG, spider=spider)
+    # return item

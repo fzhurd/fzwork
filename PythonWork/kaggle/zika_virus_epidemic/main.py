9e25fcea147cfe4c056c3e4dcbdfd1e662d6b5f3
fzhurd <minganghz@gmail.com>
2016-08-04 21:43:24 -0700

    add import files

--- PythonWork/kaggle/zika_virus_epidemic/main.py
+++ PythonWork/kaggle/zika_virus_epidemic/main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+def import_data(data_file, mode):
+    with open(data_file, mode) as f:
+        lines = f.readlines()
+        n=0
+        for l in lines:
+            n=n+1
+            if n<10:
+                print l
+
+    
+
 def main():
-    print 'hi'
+    import_data('cdc_zika.csv', 'r')
 
 
 

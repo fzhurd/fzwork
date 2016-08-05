ead5f171a9d33d258f42640f4569e612f3846130
fzhurd <minganghz@gmail.com>
2016-08-04 22:24:25 -0700

    add mongodb

--- PythonWork/kaggle/zika_virus_epidemic/main.py
+++ PythonWork/kaggle/zika_virus_epidemic/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+import pymongo
+import pymysql
+
 def import_data(data_file, mode):
     with open(data_file, mode) as f:
         lines = f.readlines()
@@ -9,6 +13,10 @@ def import_data(data_file, mode):
             n=n+1
             if n<10:
                 print l
+
+
+def set_up_mongodb_conn(host='127.0.0.1', port=27017, database='test', user=None, password=None):
+    pass
 
     
 

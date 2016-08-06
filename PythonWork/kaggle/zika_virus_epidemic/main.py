ac60f090e33e05e27e8243ace5de420b259b70ae
fzhurd <minganghz@gmail.com>
2016-08-05 23:11:30 -0700

    add strip

--- PythonWork/kaggle/zika_virus_epidemic/main.py
+++ PythonWork/kaggle/zika_virus_epidemic/main.py
@@ -9,11 +9,25 @@ def import_data(data_file, mode):
 
     db = set_up_mongodb_conn(database='zika', collection='zika_virus')
 
+    # with open(data_file, mode) as f:
+    #     for row in csv.reader(f, delimiter=' ', skipinitialspace=True):
+    #         print '|'.join(row)
 
+    # with open(data_file, mode) as f:
+    #     data = f.read()
+
+    # new_data = data.replace('"', '')
+
+    # for row in csv.reader(data.splitlines(), delimiter=' ', skipinitialspace=True):
+    #     print row
+    # print ('|'.join(row))
+    #     lines = csv.reader(csvfile)
+    #     print type(lines)
 
     with open(data_file, mode) as f:
-        lines = f.readlines()
 
+        lines=f.readlines()
+       
         n=0
         for l in lines:
             doc=dict()
@@ -22,6 +36,7 @@ def import_data(data_file, mode):
                 # print l
                 # report_date, location, location_type, data_field, data_field_code, time_period, 
                 # time_period_type, value, unit = l.strip().split(',')
+                l=l.replace('"','').strip()
                 l=l.rstrip('\n')
                 record = l.strip().split(',')
                 print record
@@ -40,6 +55,38 @@ def import_data(data_file, mode):
 
                 db.insert(doc)
             n=n+1
+
+
+
+    # with open(data_file, mode) as f:
+    #     lines = f.readlines()
+
+    #     n=0
+    #     for l in lines:
+    #         doc=dict()
+    #         # n=n+1
+    #         if n>0 and n<10:
+    #             # print l
+    #             # report_date, location, location_type, data_field, data_field_code, time_period, 
+    #             # time_period_type, value, unit = l.strip().split(',')
+    #             l=l.rstrip('\n')
+    #             record = l.strip().split(',')
+    #             print record
+
+    #             # print report_date
+
+    #             doc['report_date'] = record[0]
+    #             doc['location'] = record[1]
+    #             doc['location_type'] = record[2]
+    #             doc['data_field'] = record[3]
+    #             doc['data_field_code'] = record[4]
+    #             doc['time_period'] = record[5]
+    #             doc['time_period_type'] = record[6]
+    #             doc['value'] = record[7]
+    #             doc['unit'] = record[8]
+
+    #             db.insert(doc)
+    #         n=n+1
 
 
 def set_up_mongodb_conn(host='127.0.0.1', port=27017, database='test', 

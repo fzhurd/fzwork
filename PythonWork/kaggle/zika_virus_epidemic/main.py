616b9aa837b3018d1614ad17f8d360c8af009258
fzhurd <minganghz@gmail.com>
2016-08-07 14:56:14 -0700

    add main.py

--- PythonWork/kaggle/zika_virus_epidemic/main.py
+++ PythonWork/kaggle/zika_virus_epidemic/main.py
@@ -30,7 +30,14 @@ def import_data(data_file, mode):
                 doc['data_field_code'] = record[4]
                 doc['time_period'] = record[5]
                 doc['time_period_type'] = record[6]
-                doc['value'] = int(record[7])
+
+                try:
+                    doc['value'] = int(record[7])
+                except Exception as e:
+                    doc['value'] ="NA"
+
+                # if record[7] != "NA":
+                #     doc['value'] = int(record[7])
                 doc['unit'] = record[8]
 
                 db.insert(doc)

d1c036ae118eb9cc91a8e5274981341b9f87282f
fzhurd <minganghz@gmail.com>
2016-08-07 15:06:50 -0700

    add

--- PythonWork/kaggle/zika_virus_epidemic/main.py
+++ PythonWork/kaggle/zika_virus_epidemic/main.py
@@ -36,44 +36,11 @@ def import_data(data_file, mode):
                 except Exception as e:
                     doc['value'] ="NA"
 
-                # if record[7] != "NA":
-                #     doc['value'] = int(record[7])
+          
                 doc['unit'] = record[8]
 
                 db.insert(doc)
             n=n+1
-
-
-
-    # with open(data_file, mode) as f:
-    #     lines = f.readlines()
-
-    #     n=0
-    #     for l in lines:
-    #         doc=dict()
-    #         # n=n+1
-    #         if n>0 and n<10:
-    #             # print l
-    #             # report_date, location, location_type, data_field, data_field_code, time_period, 
-    #             # time_period_type, value, unit = l.strip().split(',')
-    #             l=l.rstrip('\n')
-    #             record = l.strip().split(',')
-    #             print record
-
-    #             # print report_date
-
-    #             doc['report_date'] = record[0]
-    #             doc['location'] = record[1]
-    #             doc['location_type'] = record[2]
-    #             doc['data_field'] = record[3]
-    #             doc['data_field_code'] = record[4]
-    #             doc['time_period'] = record[5]
-    #             doc['time_period_type'] = record[6]
-    #             doc['value'] = record[7]
-    #             doc['unit'] = record[8]
-
-    #             db.insert(doc)
-    #         n=n+1
 
 
 def set_up_mongodb_conn(host='127.0.0.1', port=27017, database='test', 
@@ -82,8 +49,6 @@ def set_up_mongodb_conn(host='127.0.0.1', port=27017, database='test',
     dbconnection = pymongo.MongoClient(host, port)
     db = dbconnection[database][collection]
     return db
-
-    
 
 def main():
     import_data('cdc_zika.csv', 'r')

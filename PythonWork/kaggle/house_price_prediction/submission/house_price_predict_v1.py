9e84b55e27f3c1c37d79e70a340a2d97905a9045
fzhurd <minganghz@gmail.com>
2016-10-09 16:07:34 -0700

    add the initial building

--- PythonWork/kaggle/house_price_prediction/submission/house_price_predict_v1.py
+++ PythonWork/kaggle/house_price_prediction/submission/house_price_predict_v1.py
@@ -10,11 +10,30 @@ from sklearn import tree
 import time
 from functools import wraps
 
-def load_train_data(train_file):
-    pass
+def monitor_time(func):
+
+    @wraps(func)
+    def calculate_time(*args, **kwargs ):
+        start_time = time.time()
+        result=func(*args, **kwargs)
+        end_time=time.time()
+        cost_time=end_time-start_time
+        print(cost_time)
+        return result
+
+    return calculate_time
+
+def load_train_data(file):
+
+    train=pd.read_csv(file)
+    return train
+    
 
 def main():
-    load_train_data('../input/train.csv')
+    train=load_train_data('../input/train.csv')
+    print train.head(3)
+
+    test=load_train_data('../input/test.csv')
 
 if __name__ == '__main__':
     main()

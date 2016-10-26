739b0e82bb3562aa463e41e5245d975409fe736f
fzhurd <minganghz@gmail.com>
2016-10-25 22:40:23 -0700

    add import

---
+++ PythonWork/datascience/data_mining_project2/relator/relator/pipelines.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymongo
+ 
+from scrapy.conf import settings
+
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+class RelatorPipeline(object):
+    
+    def __init__(self):
+        self.file = open('items.jl', 'wb')
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+    # def process_item(self, item, spider):
+    #     return item
+
+
+class MongoDBPipeline(object):
+
+    collection_name = 'properties'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGODB_SERVER'),
+            mongo_db=crawler.settings.get('MONGODB_DB', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert(dict(item))
+        return item
---
+++ PythonWork/datascience/data_mining_project2/relator/relator/spiders/relator_spider.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from scrapy import Spider
+from scrapy.selector import Selector
+from relator.items import RelatorItem
+
+
+class RelatorSpider(Spider):
+
+    name = "relator_spider"
+    allowed_domains = ["www.relator.ca"]
+    start_urls = [
+        "https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-123.29681396484376&LongitudeMax=-122.96550750732423&LatitudeMin=49.11287172635217&LatitudeMax=49.20828967427575&SortOrder=A&SortBy=1&viewState=m&Longitude=-123.131160736084&Latitude=49.1606036799933&ZoomLevel=12&PropertyTypeGroupID=1"
+        #"https://www.realtor.ca"
+        # "https://www.realtor.ca/Commercial/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=0&TransactionTypeId=2&PropertyTypeGroupID=2&LongitudeMin=-123.30780029296876&LongitudeMax=-122.95452117919923&LatitudeMin=49.1046684354908&LatitudeMax=49.216475820806444&SortOrder=A&SortBy=1&viewState=m&CurrentPage=2",
+    ]
+# https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-123.29681396484376&LongitudeMax=-122.96550750732423&LatitudeMin=49.11287172635217&LatitudeMax=49.20828967427575&SortOrder=A&SortBy=1&viewState=m&Longitude=-123.131160736084&Latitude=49.1606036799933&ZoomLevel=12&PropertyTypeGroupID=1
+    def parse(self, response):
+        pass
+
+        # houses = Selector(response).xpath('//div[@class="m_map_map_cnt_lst_cnt_plchldr"]/')
+        # print houses
+
+        # for h in houses:
+
+        #     item = StackworkItem()
+
+        #     item['title'] = h.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
+
+        #     item['url'] = h.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+
+        #     yield item
+
+        # questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        # for question in questions:
+
+        #     item = StackworkItem()
+
+        #     item['title'] = question.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
+
+        #     item['url'] = question.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+
+        #     yield item

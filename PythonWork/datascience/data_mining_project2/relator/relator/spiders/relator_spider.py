b1d731ccee8b1c954afee02b19a580159f08fdcc
fzhurd <minganghz@gmail.com>
2016-11-06 22:33:47 -0800

    add running command example

--- PythonWork/datascience/data_mining_project2/relator/relator/spiders/relator_spider.py
+++ PythonWork/datascience/data_mining_project2/relator/relator/spiders/relator_spider.py
@@ -5,6 +5,8 @@ from scrapy import Spider
 from scrapy.selector import Selector
 from relator.items import RelatorItem
 
+# scrapy crawl relator_spider
+# scrapy crawl relator_spider -o items.json -t json
 
 class RelatorSpider(Spider):
 
@@ -16,10 +18,10 @@ class RelatorSpider(Spider):
 
     start_urls = [
     # 'http://quotes.toscrape.com/page/1/',
-        # "https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-123.29681396484376&LongitudeMax=-122.96550750732423&LatitudeMin=49.140954912790704&LatitudeMax=49.18024465477734&SortOrder=A&SortBy=1&viewState=m&Longitude=-123.131160736084&Latitude=49.1606036799933&ZoomLevel=12&CurrentPage=1&PropertyTypeGroupID=1"
+        "https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-123.29681396484376&LongitudeMax=-122.96550750732423&LatitudeMin=49.11287172635217&LatitudeMax=49.20828967427575&SortOrder=A&SortBy=1&viewState=m&Longitude=-123.131160736084&Latitude=49.1606036799933&ZoomLevel=12&PropertyTypeGroupID=1"
         # "http://stackoverflow.com"
         # "https://www.realtor.ca/Residential/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=2&StoreyRange=0-0&BedRange=0-0&BathRange=0-0&LongitudeMin=-123.29681396484376&LongitudeMax=-122.96550750732423&LatitudeMin=49.11287172635217&LatitudeMax=49.20828967427575&SortOrder=A&SortBy=1&viewState=m&Longitude=-123.131160736084&Latitude=49.1606036799933&ZoomLevel=12&PropertyTypeGroupID=1"
-        "https://www.realtor.ca/"
+        # "https://www.realtor.ca/"
         # "https://www.realtor.ca/Commercial/Map.aspx#CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=0&TransactionTypeId=2&PropertyTypeGroupID=2&LongitudeMin=-123.30780029296876&LongitudeMax=-122.95452117919923&LatitudeMin=49.1046684354908&LatitudeMax=49.216475820806444&SortOrder=A&SortBy=1&viewState=m&CurrentPage=2",
     ]
 
@@ -28,19 +30,19 @@ class RelatorSpider(Spider):
 
     def parse(self, response):
 
-        # houses = Selector(response).xpath('//div[@class="m_map_map_cnt_lst_cnt_plchldr"]/')
-        houses = Selector(response).xpath('//div')
+        houses = Selector(response).xpath('//div[@class="m_map_map_cnt_lst_cnt_plchldr"]/')
+        # houses = Selector(response).xpath('//div')
         print houses
 
-        for h in houses:
+        # for h in houses:
 
-            item = StackworkItem()
+        #     item = StackworkItem()
 
-            item['title'] = h.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
+        #     item['title'] = h.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
 
-            item['url'] = h.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+        #     item['url'] = h.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
 
-            yield item
+        #     yield item
 
 
     # def parse(self, response):

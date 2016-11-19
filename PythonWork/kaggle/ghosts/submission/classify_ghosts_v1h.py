92982e52fdc862ba3947f4f9d6f9a5a3c2c5167c
fzhurd <minganghz@gmail.com>
2016-11-19 14:50:00 -0800

    add initial commit

--- PythonWork/kaggle/ghosts/submission/classify_ghosts_v1h.py
+++ PythonWork/kaggle/ghosts/submission/classify_ghosts_v1h.py
@@ -1,9 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+from pandas import Series,DataFrame
+from numpy import *  
+import csv  
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+# from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import BernoulliRBM
+from sklearn import tree
+from sklearn import svm
+import time
+from functools import wraps
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.decomposition import PCA
+
+def monitor_time(func):
+
+    @wraps(func)
+    def calculate_time(*args, **kwargs ):
+        start_time = time.time()
+        result=func(*args, **kwargs)
+        end_time=time.time()
+        cost_time=end_time-start_time
+        print(cost_time)
+        return result
+
+    return calculate_time
+
 
 def main():
-    print 'ghosts'
+    df_train=pd.read_csv('../input/train.csv')
+
+    print df_train.head(3)
+    df_test=pd.read_csv('../input/test.csv')
+    print df_test.head(3)
+
 
 
 if __name__=='__main__':

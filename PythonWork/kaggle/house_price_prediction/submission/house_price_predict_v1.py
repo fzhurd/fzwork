5b2698c550dcb560592b7b3f58801942fee18b30
fzhurd <minganghz@gmail.com>
2016-12-08 23:12:32 -0800

    use sns

--- PythonWork/kaggle/house_price_prediction/submission/house_price_predict_v1.py
+++ PythonWork/kaggle/house_price_prediction/submission/house_price_predict_v1.py
@@ -33,48 +33,57 @@ def load_data(file):
 def main():
     train=load_data('../input/train.csv')
     print train.head(3)
-    print train.shape
-    print train.describe()
-    print pd.isnull(train).any()
-    print train.mean()
+    # print train.shape
+    # print train.describe()
+    # print pd.isnull(train).any()
+    # print train.mean()
 
     train.fillna(train.mean())
 
-    y=train["SalePrice"]
-    train.drop("SalePrice", axis=1, inplace=True)
+    sns.set()
+    sns.pairplot(train[["MSSubClass", "MSZoning",  "LotFrontage" ]], hue=train[["SalePrice"]])
+    sns.plt.show()
 
-    print train.corr(method='pearson')
+    # y=train["SalePrice"]
+    # train.drop("SalePrice", axis=1, inplace=True)
 
-    pearson=train.corr(method='pearson')
-    corr_with_target=pearson.ix[-1][:-1]
-    corr_with_target_dict=corr_with_target.to_dict()
+    # print train.corr(method='pearson')
 
-    for attr in sorted(corr_with_target_dict.items(), key = lambda x: -abs(x[1])):
-        print("{0}: \t{1}".format(*attr))
+    # pearson=train.corr(method='pearson')
+    # corr_with_target=pearson.ix[-1][:-1]
+    # corr_with_target_dict=corr_with_target.to_dict()
 
-    # corr_with_target[abs(corr_with_target).argsort()[::1]]
+    # for attr in sorted(corr_with_target_dict.items(), key = lambda x: -abs(x[1])):
+    #     print("{0}: \t{1}".format(*attr))
 
-    attrs = pearson.iloc[:-1,:-1] # all except target
-    # only important correlations and not auto-correlations
-    threshold = 0.5
-    # {(YearBuilt, YearRemodAdd): 0.592855, (1stFlrSF, GrLivArea): 0.566024, ...
-    important_corrs = (attrs[abs(attrs) > threshold][attrs != 1.0]) \
-        .unstack().dropna().to_dict()
-    #     attribute pair                   correlation
-    # 0     (OverallQual, TotalBsmtSF)     0.537808
-    # 1     (GarageArea, GarageCars)       0.882475
-    # ...
-    unique_important_corrs = pd.DataFrame(
-        list(set([(tuple(sorted(key)), important_corrs[key]) \
-        for key in important_corrs])), columns=['Attribute Pair', 'Correlation'])
-    # sorted by absolute value
-    unique_important_corrs = unique_important_corrs.ix[
-        abs(unique_important_corrs['Correlation']).argsort()[::-1]]
+    # # corr_with_target[abs(corr_with_target).argsort()[::1]]
 
-    test=load_data('../input/test.csv')
+    # attrs = pearson.iloc[:-1,:-1] # all except target
+    # # only important correlations and not auto-correlations
+    # threshold = 0.5
+    # # {(YearBuilt, YearRemodAdd): 0.592855, (1stFlrSF, GrLivArea): 0.566024, ...
+    # important_corrs = (attrs[abs(attrs) > threshold][attrs != 1.0]) \
+    #     .unstack().dropna().to_dict()
+    # #     attribute pair                   correlation
+    # # 0     (OverallQual, TotalBsmtSF)     0.537808
+    # # 1     (GarageArea, GarageCars)       0.882475
+    # # ...
+    # unique_important_corrs = pd.DataFrame(
+    #     list(set([(tuple(sorted(key)), important_corrs[key]) \
+    #     for key in important_corrs])), columns=['Attribute Pair', 'Correlation'])
+    # # sorted by absolute value
+    # unique_important_corrs = unique_important_corrs.ix[
+    #     abs(unique_important_corrs['Correlation']).argsort()[::-1]]
 
-    print 'from test'
-    print test.head(3), 'ttttttttttttttttttttttt'
+
+    # # sns.set()
+    # # sns.pairplot(train.head(100)[["MSSubClass", "MSZoning",  "LotFrontage" ]], hue="SalePrice")
+    # # sns.plt.show()
+
+    # test=load_data('../input/test.csv')
+
+    # print 'from test'
+    # print test.head(3), 'ttttttttttttttttttttttt'
     # print test.shape
     # print test.describe()
     # print pd.isnull(test).any()

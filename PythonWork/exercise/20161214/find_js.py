771084695f8f83c4580b705f6c229d3e7b62f2ad
fzhurd <minganghz@gmail.com>
2016-12-14 23:48:09 -0800

    temp

--- PythonWork/exercise/20161214/find_js.py
+++ PythonWork/exercise/20161214/find_js.py
@@ -15,32 +15,40 @@ import re
 
 
 from collections import Counter
+
+def find_test(element):
+    if '.js' in element:
+        return element
+
+
 def read_file(file, mode):
+
 
     # regex=re.compile("*.js")
     tests=[]
     with open(file,mode) as fn:
         for f in fn:
             line=f.split()
-
-            # print '**************'
-
             for l in line:
-                ele=l.split('/')              
-                # x=[m.group(0) for l in ele for m in [regex.search(l)] if m]
-                # print x
-                for e in ele:
-                    if '.js' in e:
-                        print e
-                        tests.append(e)
+                ele=l.split('/')  
+                # tests=map(find_test, ele)
+                temp= [e.upper() for e in ele if '.js' in e]
+                print temp
+                # if temp is not None:
+                #     tests.append(temp)
+                
+
+                # for e in ele:
+                #     if '.js' in e:
+                #         tests.append(e)
         return tests
 
 
 def main():
     tests=read_file('./temp.txt', 'r')
-    print tests
+    # print tests
     tim=Counter(tests)
-    print tim
+    # print tim
 
     for k, v in tim.iteritems():
         if v==2:

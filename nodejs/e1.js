17296e56ccea1d41c2adbd33464a683449bb6998
fzhurd <minganghz@gmail.com>
2017-03-07 22:38:24 -0800

    add

---
+++ nodejs/e1.js
@@ -0,0 +1,15 @@
+class Employee {
+	constructor(){
+		this.firstName="john";
+		this.lastName="wong";
+	}
+
+	printinfo(){
+		console.log(this.firstName+" "+this.lastName);
+	}
+
+
+}
+
+e1=new Employee("Jerry", "Zhang");
+e1.printinf();

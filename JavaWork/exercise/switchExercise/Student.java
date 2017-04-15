f288f52589426191bcd4a19c45ee8108a2a26ce0
fzhurd <minganghz@gmail.com>
2017-04-14 20:14:20 -0700

    add helper class

---
+++ JavaWork/exercise/switchExercise/Student.java
@@ -0,0 +1,32 @@
+package com.fz.automation;
+
+public class Student {
+	
+	private String name;
+	private int number;
+
+	public Student(String name, int number) {
+		this.name=name;
+		this.number=number;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+
+
+}

96b40a0c95351e2b5bfc5aefe885d78363651076
fzhurd <minganghz@gmail.com>
2017-05-14 19:21:01 -0700

    initial push

---
+++ PythonWork/kaggle/sberbank_russian_house_market/submission/predict_russian_house_market_v1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# import pandas as pd
+# from pandas import Series,DataFrame
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn import cross_validation
+# from sklearn import tree
+# import time
+# from functools import wraps
+# import seaborn as sns
+
+def main():
+    print 'test'
+
+
+if __name__=="__main__":
+    main()

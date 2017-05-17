f0c1dbff8da75db100ee732ae9fc912fc84cef58
fzhurd <minganghz@gmail.com>
2017-05-16 23:04:10 -0700

    add load function

--- PythonWork/kaggle/sberbank_russian_house_market/submission/predict_russian_house_market_v1.py
+++ PythonWork/kaggle/sberbank_russian_house_market/submission/predict_russian_house_market_v1.py
@@ -7,8 +7,29 @@ from numpy.polynomial.chebyshev import *
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+def monitor_time(func):
+
+    @wraps(func)
+    def calculate_time(*args, **kwargs ):
+        start_time = time.time()
+        result=func(*args, **kwargs)
+        end_time=time.time()
+        cost_time=end_time-start_time
+        print(cost_time)
+        return result
+
+    return calculate_time
+
+def load_data(file):
+
+    df=pd.read_csv(file)
+    return df
+
 def main():
-    print 'test'
+    train=load_data('../input/train.csv')
+    print train.head(5)
+    print train.shape
 
 
 if __name__=="__main__":

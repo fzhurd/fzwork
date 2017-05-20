59134f2d1fa4b782685f18d0924368562641a976
fzhurd <minganghz@gmail.com>
2017-05-20 14:01:58 -0700

    minor changes

--- PythonWork/kaggle/sberbank_russian_house_market/submission/predict_russian_house_market_v1.py
+++ PythonWork/kaggle/sberbank_russian_house_market/submission/predict_russian_house_market_v1.py
@@ -78,6 +78,7 @@ def main():
                 'salary_growth', 'unemployment', 'average_provision_of_build_contract_moscow', 'mortgage_rate', \
                  'deposits_rate','deposits_growth','rent_price_3room_eco',\
                  'rent_price_3room_bus']
+
     train_df = pd.read_csv("../input/train.csv",usecols=['timestamp','price_doc','full_sq'])
     macro_df = pd.read_csv("../input/macro.csv",usecols=read_columns)
 
@@ -121,6 +122,53 @@ def main():
     print ('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
     ### Read Data for macro analysis
     anlz_df=init_anlz_file(train_df, read_columns)
+
+
+    methods=['pearson', 'kendall', 'spearman']
+    def plot_grouped_trends(df,feat1,feat2,corr_df):
+       
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+        x=df.index.values
+        ch=chebfit(x,df[feat1].values,7)
+        trendf1=chebval(x,ch)
+        ax[0].plot(x,df[feat1].values,x,trendf1)
+        ax[0].set_ylabel(feat1)
+        ax[0].set_title('Chart '+feat1+' vs trend' )
+        ax[0].set_xlabel('months count')
+        ch2=chebfit(x,df[feat2].values,7)
+        trendf2=chebval(x,ch2)
+        ax[1].plot(x,df[feat2].values,x,trendf2)
+        ax[1].set_ylabel(feat2)
+        ax[1].set_title('Chart '+feat2+' vs trend' )
+        ax[1].set_xlabel('months count')
+        ##### do here two charts density distribition
+        
+        ls=[feat2]
+        for method in methods:
+            corr=df[[feat1,feat2]].corr(method=method)
+            ls.append(corr[feat1][1])
+        corr_df.loc[len(corr_df)]=ls
+
+
+    anlz_df['timestamp']=pd.to_datetime(anlz_df['timestamp'])
+    anlz_df['mo_ye']=anlz_df['timestamp'].apply(lambda x: x.strftime('%m-%Y'))
+    anlz_df['price_per_sqm']=anlz_df['price_doc']/anlz_df['full_sq']
+
+
+    macro_columns = ['price_doc','price_per_sqm','full_sq','oil_urals', 'gdp_quart_growth', 'cpi', 'usdrub', \
+                    'salary_growth', 'unemployment', 'average_provision_of_build_contract_moscow', 'mortgage_rate', \
+                     'deposits_rate','deposits_growth','rent_price_3room_eco',\
+                     'rent_price_3room_bus']
+    macro_df=pd.DataFrame(anlz_df.groupby('mo_ye')[macro_columns].mean())
+    macro_df.reset_index(inplace=True)
+
+
+    macro_df['mo_ye']=pd.to_datetime(macro_df['mo_ye'])
+    macro_df=macro_df.sort_values(by='mo_ye')
+
+
+    macro_df.reset_index(inplace=True)
+    macro_df.drop(['index'],axis=1,inplace=True)
 
 
 if __name__=="__main__":

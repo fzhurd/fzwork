0ddb476d34caa02e6e00816006ce5c03e13592b5
fzhurd <minganghz@gmail.com>
2017-07-01 20:13:05 -0700

    initial push

---
+++ PythonWork/kaggle/mercedes_benz_greener_manufacturing/submission/mercedes_benz_manufacture_v1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pd 
+from subprocess import check_output
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBRegressor
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score
+
+
+def main():
+    print "test"
+
+
+if __name__=="__main__":
+    main()

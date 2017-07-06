3f8ca6cd563412ba2b38e97cea8270482f8fe1ad
fzhurd <minganghz@gmail.com>
2017-07-05 23:18:39 -0700

    add

--- PythonWork/kaggle/movie/submission/movie_data_handle_v1.py
+++ PythonWork/kaggle/movie/submission/movie_data_handle_v1.py
@@ -14,6 +14,18 @@ def main():
     movie_raw_data = pd.read_csv('../input/movie_metadata.csv')
     print movie_raw_data.head(3)
 
+    print movie_raw_data.isnull().sum()
+
+    print movie_raw_data.shape
+
+
+
+    # movie_filterd_imdbscore=movie_raw_data['imdb_score'].loc
+    # movie_filterd_imdbscore=movie_raw_data.loc[movie_raw_data['imdb_score'].isin([2,3])]
+    movie_filterd_imdbscore=movie_raw_data.loc[movie_raw_data['imdb_score'] >0 & movie_raw_data['imdb_score']<10]
+
+    print movie_filterd_imdbscore.shape
+
 
 if __name__=='__main__':
     main()

1ceb030e2e662e5253c051b95974e00f2714c0b1
fzhurd <minganghz@gmail.com>
2017-07-07 22:57:25 -0700

    initial push

---
+++ PythonWork/kaggle/personal_medicine_cancer_treatment/submission/pmrct_v1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pd 
+from subprocess import check_output
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score
+import xgboost as xgb
+from sklearn.decomposition import PCA, FastICA
+
+
+def main():
+    print 'test'
+
+
+if __name__=='__main__':
+    main()

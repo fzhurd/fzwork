efa4b17c3bec87181a42f8b89d6a1fb06b760c81
fzhurd <minganghz@gmail.com>
2017-08-12 22:50:12 -0700

    test data filter

--- PythonWork/kaggle/movie/submission/movie_data_handle_v1.py
+++ PythonWork/kaggle/movie/submission/movie_data_handle_v1.py
@@ -12,19 +12,26 @@ from sklearn.decomposition import PCA, FastICA
 
 def main():
     movie_raw_data = pd.read_csv('../input/movie_metadata.csv')
-    print movie_raw_data.head(3)
+    # print movie_raw_data.head(3)
 
-    print movie_raw_data.isnull().sum()
+    # print movie_raw_data.isnull().sum()
 
-    print movie_raw_data.shape
+    # print movie_raw_data.shape
 
 
 
     # movie_filterd_imdbscore=movie_raw_data['imdb_score'].loc
     # movie_filterd_imdbscore=movie_raw_data.loc[movie_raw_data['imdb_score'].isin([2,3])]
-    movie_filterd_imdbscore=movie_raw_data.loc[movie_raw_data['imdb_score'] >0 & movie_raw_data['imdb_score']<10]
+    movie_filterd_imdbscore_first=movie_raw_data.loc[movie_raw_data['imdb_score'] >5]
+    movie_filterd_imdbscore_from_raw=movie_raw_data.loc[movie_raw_data['imdb_score'] <8]
 
-    print movie_filterd_imdbscore.shape
+    print movie_filterd_imdbscore_first.shape
+
+
+    movie_filterd_imdbscore_second=movie_filterd_imdbscore_first.loc[movie_raw_data['imdb_score'] <8]
+
+    print movie_filterd_imdbscore_second.shape
+    print movie_filterd_imdbscore_from_raw.shape
 
 
 if __name__=='__main__':

2a5606a5abc7c9b006868874122d47e3dda4874a
fzhurd <minganghz@gmail.com>
2017-08-12 23:32:32 -0700

    first example of data analysis

--- PythonWork/kaggle/movie/submission/movie_data_handle_v1.py
+++ PythonWork/kaggle/movie/submission/movie_data_handle_v1.py
@@ -16,12 +16,16 @@ def main():
 
     # print movie_raw_data.isnull().sum()
 
-    # print movie_raw_data.shape
+    print movie_raw_data.shape
+    movie_raw_data_dropna=movie_raw_data.dropna()
+    print movie_raw_data_dropna.shape
+    print movie_raw_data.dtypes
 
 
 
     # movie_filterd_imdbscore=movie_raw_data['imdb_score'].loc
     # movie_filterd_imdbscore=movie_raw_data.loc[movie_raw_data['imdb_score'].isin([2,3])]
+
     movie_filterd_imdbscore_first=movie_raw_data.loc[movie_raw_data['imdb_score'] >5]
     movie_filterd_imdbscore_from_raw=movie_raw_data.loc[movie_raw_data['imdb_score'] <8]
 
@@ -33,6 +37,15 @@ def main():
     print movie_filterd_imdbscore_second.shape
     print movie_filterd_imdbscore_from_raw.shape
 
+    print '*********************************'
+
+    print movie_raw_data_dropna.head(3)
+    profit=(((movie_raw_data_dropna['gross'].values-movie_raw_data_dropna['budget'].values))/(movie_raw_data_dropna['gross'].values))*100
+    print profit
+
+    movie_raw_data_dropna.loc[:,'profit']=pd.Series(profit, movie_raw_data_dropna.index)
+    print movie_raw_data_dropna.shape
+    print movie_raw_data_dropna.head(3)
 
 if __name__=='__main__':
     main()

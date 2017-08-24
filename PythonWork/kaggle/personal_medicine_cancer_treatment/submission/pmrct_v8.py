cb159c75fd7d730e30d82a7079d20fd8914ff3df
fzhurd <minganghz@gmail.com>
2017-08-24 00:14:11 -0700

    add axis

--- PythonWork/kaggle/personal_medicine_cancer_treatment/submission/pmrct_v8.py
+++ PythonWork/kaggle/personal_medicine_cancer_treatment/submission/pmrct_v8.py
@@ -15,6 +15,8 @@ test_text = pd.read_csv("../input/test_text", sep="\|\|", engine='python', heade
 train = pd.merge(train_variant, train_text, how='left', on='ID')
 x_train = train.drop('Class', axis=1)
 
-testing_variants_df = pd.read_csv("../input/test_variants")
-testing_text_df = pd.read_csv("../input/test_text", sep="\|\|", engine='python', header=None, skiprows=1, names=["ID","Text"])
-testing_merge_df = testing_variants_df.merge(testing_text_df,left_on="ID",right_on="ID")
+x_test = pd.merge(test_variant, test_text, how='left', on='ID')
+
+data = np.concatenate((x_train, x_test), axis=0)
+data=pd.DataFrame(data)
+data.columns = ["ID", "Gene", "Variation", "Text"]

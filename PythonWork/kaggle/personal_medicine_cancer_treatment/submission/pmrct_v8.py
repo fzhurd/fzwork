20abc2c3b3a32dd27d846ad1df6cccda97d46c7d
fzhurd <minganghz@gmail.com>
2017-08-28 19:43:46 -0700

    added

--- PythonWork/kaggle/personal_medicine_cancer_treatment/submission/pmrct_v8.py
+++ PythonWork/kaggle/personal_medicine_cancer_treatment/submission/pmrct_v8.py
@@ -28,27 +28,40 @@ data.columns = ["ID", "Gene", "Variation", "Text"]
 
 print data.head(3)
 
+
+from sklearn.model_selection import train_test_split
+
+train ,test = train_test_split(train,test_size=0.2) 
+
+
+x_train = train['Text'].values
+x_test = test['Text'].values
+y_train = train['Class'].values
+y_test = test['Class'].values
+
+
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer()),
                      ('clf', svm.LinearSVC())
 ])
 
 y_train = train['Class']
-text_clf = text_clf.fit(x_train,y_train)
+print y_train.head(10)
+text_clf = text_clf.fit(x_train, y_train)
 
 y_test_predicted = text_clf.predict(x_test)
 # np.mean(y_test_predicted == y_test)
 print y_test_predicted
 
 
-X_test_final = testing_merge_df['Text'].values
+# X_test_final = testing_merge_df['Text'].values
 
-predicted_class = text_clf.predict(X_test_final)
+# predicted_class = text_clf.predict(X_test_final)
 
-testing_merge_df['predicted_class'] = predicted_class
+# testing_merge_df['predicted_class'] = predicted_class
 
-Appended the predicted values to the testing data
 
-testing_merge_df.head(5)
+
+# testing_merge_df.head(5)
 
 

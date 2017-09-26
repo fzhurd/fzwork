1d0ec4a54370198517befe931fbd9fba9d776660
fzhurd <minganghz@gmail.com>
2017-09-25 23:47:38 -0700

    add

--- PythonWork/kaggle/predict_van_house/crawler/crawler_v1.py
+++ PythonWork/kaggle/predict_van_house/crawler/crawler_v1.py
@@ -6,6 +6,19 @@ import pandas as pd
 
 import urllib2
 
-# response=urllib2.urlopen("https://www.realtor.ca")
-response=urllib2.urlopen("https://www.google.ca")
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7'}
+# url = "https://www.realtor.ca"
+url = "https://www.rew.ca/properties/areas/vancouver-bc"
+
+request = urllib2.Request(url)
+response = urllib2.urlopen(request)
 print response.read()
+
+# import requests
+# from bs4 import BeautifulSoup
+# request_page = requests.get(url)
+# soup = BeautifulSoup(request_page.text, 'lxml')
+
+
+# for i in soup.findAll('li'):
+#     print(i.text)

59203a21f4020a7860037e502963ebf4451c631e
fzhurd <minganghz@gmail.com>
2017-10-09 00:17:11 -0700

    add the check of null value

--- PythonWork/kaggle/porto_segru_safe_driver_prediction/submission/pssdp_v1.py
+++ PythonWork/kaggle/porto_segru_safe_driver_prediction/submission/pssdp_v1.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pymongo
+import numpy as np 
+import pandas as pd 
+import re
+
+import urllib2
 
 def main():
-    print 'test'
+    train_raw_data = pd.read_csv("../input/train.csv")
+    test_raw_data = pd.read_csv("../input/test.csv")
+
+    print 'train data shape:'+ str(train_raw_data.shape)
+    print 'test data shape:'+ str(test_raw_data.shape)
+
+    print train_raw_data.isnull().sum()
+    print test_raw_data.isnull().sum()
 
 
 if __name__=='__main__':
+
     main()

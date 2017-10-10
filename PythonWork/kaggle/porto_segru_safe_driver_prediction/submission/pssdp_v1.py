1dfa7460066fbf6e9fbd6aaac9cbf69b2b2d0a4d
fzhurd <minganghz@gmail.com>
2017-10-09 21:06:46 -0700

    add calculating the -1

--- PythonWork/kaggle/porto_segru_safe_driver_prediction/submission/pssdp_v1.py
+++ PythonWork/kaggle/porto_segru_safe_driver_prediction/submission/pssdp_v1.py
@@ -5,6 +5,8 @@ import pymongo
 import numpy as np 
 import pandas as pd 
 import re
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import urllib2
 
@@ -17,6 +19,15 @@ def main():
 
     print train_raw_data.isnull().sum()
     print test_raw_data.isnull().sum()
+
+    missing_df = np.sum(train_raw_data==-1, axis=0)
+    print missing_df.head(10)
+    missing_df.sort_values(ascending=False, inplace=True)
+    plt.figure(figsize=(10, 20))
+    sns.barplot(x=missing_df.values, y=missing_df.index)
+    plt.title("Number of missing values in each column")
+    plt.xlabel("Count of missing values")
+    plt.show()
 
 
 if __name__=='__main__':

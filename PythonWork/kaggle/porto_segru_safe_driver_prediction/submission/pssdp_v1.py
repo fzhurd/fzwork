9a20bdbde4d102b679b9501753f84b63365cc94e
fzhurd <minganghz@gmail.com>
2017-10-22 22:47:04 -0700

    add gini

--- PythonWork/kaggle/porto_segru_safe_driver_prediction/submission/pssdp_v1.py
+++ PythonWork/kaggle/porto_segru_safe_driver_prediction/submission/pssdp_v1.py
@@ -10,6 +10,25 @@ import matplotlib.pyplot as plt
 
 import urllib2
 
+# Compute gini
+
+# from CPMP's kernel https://www.kaggle.com/cpmpml/extremely-fast-gini-computation
+@jit
+def eval_gini(y_true, y_prob):
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_prob)]
+    ntrue = 0
+    gini = 0
+    delta = 0
+    n = len(y_true)
+    for i in range(n-1, -1, -1):
+        y_i = y_true[i]
+        ntrue += y_i
+        gini += y_i * delta
+        delta += 1 - y_i
+    gini = 1 - 2 * gini / (ntrue * (n - ntrue))
+    return gini
+
 def main():
     train_raw_data = pd.read_csv("../input/train.csv")
     test_raw_data = pd.read_csv("../input/test.csv")

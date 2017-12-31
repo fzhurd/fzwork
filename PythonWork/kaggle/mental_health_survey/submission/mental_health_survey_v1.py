d7d9a7aa05e0319daa70ae5dbf485cf717de59cb
fzhurd <minganghz@gmail.com>
2017-12-31 15:02:59 -0800

    add

--- PythonWork/kaggle/mental_health_survey/submission/mental_health_survey_v1.py
+++ PythonWork/kaggle/mental_health_survey/submission/mental_health_survey_v1.py
@@ -17,6 +17,10 @@ import matplotlib.pyplot as plt
 
 survey_data=pd.read_csv('../input/survey.csv')
 
+print "*"*60
+print "This is the template for general data analyisis"
+print "*"*60
+
 print 'survey_data shapes:'
 print survey_data.shape
 
@@ -25,4 +29,35 @@ print survey_data.columns
 
 print 'survey_data first 3 rows'
 print survey_data.head(3)
+
+print 'survey_data columns with null value'
+print survey_data.isnull().sum()
+
+
+# var='family_history'
+
+# survey_data.plot.scatter(x=var, y='treatment')
+
+survey_data=survey_data.drop('work_interfere', axis=1)
+survey_data=survey_data.drop('state', axis=1)
+survey_data=survey_data.drop('comments', axis=1)
+survey_data=survey_data.drop('self_employed', axis=1)
+
+print survey_data.shape
+print survey_data.isnull().sum()
+
+survey_data_cat_features=survey_data.select_dtypes(include=['object']).columns
+print survey_data_cat_features
+
+survey_data_numeric_features=survey_data.select_dtypes(exclude=['object']).columns
+print survey_data_numeric_features
+
+
+corr=survey_data.corr()
+f, ax = plt.subplots(figsize=(11, 9))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(corr, cmap=cmap, vmax=1,
+            square=True,
+            linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
+# plt.show()
 

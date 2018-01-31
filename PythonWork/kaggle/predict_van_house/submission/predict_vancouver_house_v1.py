5a6cf0fd53d723c3e6122417480a024547526ed4
fzhurd <minganghz@gmail.com>
2018-01-30 22:35:34 -0800

    add file

---
+++ PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# mongoexport --host 127.0.0.1 --db vancouver_properties --collection properties_info --type=csv --out vancouver_houses.csv --fields _id,info,features,sqrt,bath,bed,address
+print "test"

a130a735957db94aad48b357c51dd07604c04d59
fzhurd <minganghz@gmail.com>
2018-01-30 23:45:34 -0800

    add data

--- PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
+++ PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+import numpy as np
 # mongoexport --host 127.0.0.1 --db vancouver_properties --collection properties_info --type=csv --out vancouver_houses.csv --fields _id,info,features,sqrt,bath,bed,address
-print "test"
+# db.properties_info2.update({},{$rename:{'price':'address'}},false, true)
+
+
+houses=pd.read_csv('../input/vancouver_houses.csv')
+print houses.head(3)
+
+print houses.shape
+print houses.isnull().sum()
+
+print int(houses['info'].split(',').join())
+

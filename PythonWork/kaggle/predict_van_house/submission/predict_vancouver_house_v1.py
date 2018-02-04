064d90e9990aac10dd0779ba3500cf865ed194a8
fzhurd <minganghz@gmail.com>
2018-02-03 23:29:08 -0800

    add

--- PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
+++ PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 # mongoexport --host 127.0.0.1 --db vancouver_properties --collection properties_info --type=csv --out vancouver_houses.csv --fields _id,info,features,sqrt,bath,bed,address
 # db.properties_info2.update({},{$rename:{'price':'address'}},false, true)
 
@@ -18,6 +19,13 @@ f = lambda x: int(x['info'].replace(",",""))
 houses['info']=houses.apply(f, axis=1)
 
 print houses['info']
-
 print houses.head(3)
+# print houses['info'].max
+# print houses['info'].min
+# print houses.groupby('info').agg({'info':['max', 'mean']})
+print houses.max()
+
+plt.hist(houses['info'], 10)
+plt.xlim(100000, 38000000)
+plt.show()
 

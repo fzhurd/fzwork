7f70aa5e731da3c8088d58cc8bb2375c2ea940cf
fzhurd <minganghz@gmail.com>
2018-02-06 23:40:38 -0800

    add

--- PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
+++ PythonWork/kaggle/predict_van_house/submission/predict_vancouver_house_v1.py
@@ -21,22 +21,43 @@ houses['info']=houses.apply(f, axis=1)
 
 print houses['info']
 print houses.head(3)
-# print houses['info'].max
-# print houses['info'].min
-# print houses.groupby('info').agg({'info':['max', 'mean']})
+
 print houses.max()
+print houses.min()
 
+# get the histogram
+plt.hist(houses['info'], 100)
+plt.xlim(0, 38000000)
+plt.show()
 
-plt.hist(houses['info'], 50)
-plt.xlim(1000000, 38000000)
+# print the correlation and the n largest feature related to info
+corr = houses.corr()
+print corr, 'ccc'
+print corr.nlargest(2, 'info'),
+
+get distribution plot
+sns.distplot(houses['info'])
+plt.show()
+
+print houses['info'].skew()
+print houses['info'].kurt()
+
+# To check whether it is linear relation
+var = 'sqrt'
+data = pd.concat([houses['info'], houses[var]], axis=1)
+data.plot.scatter(x=var, y='info', ylim=(0,38000000))
+plt.show()
+
+# bath number is not linear relation with price
+var = 'bath'
+data = pd.concat([houses['bath'], houses[var]], axis=1)
+data.plot.scatter(x=var, y='bath', ylim=(0,38000000))
 plt.show()
 
 
-corr = houses.corr()
-print corr
-
-
-sns.distplot(houses['info'])
+var = 'bath'
+data = pd.concat([houses['bath'], houses[var]], axis=1)
+sns.boxplot(x=var, y='bath', ylim=(0,38000000))
 plt.show()
 
 

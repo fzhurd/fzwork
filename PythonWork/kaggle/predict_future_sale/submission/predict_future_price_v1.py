80483b5fb837f46b5d99694456cdc78d13aedbf2
fzhurd <minganghz@gmail.com>
2018-04-16 23:48:25 -0700

    initial push to read data

---
+++ PythonWork/kaggle/predict_future_sale/submission/predict_future_price_v1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+
+def main():
+    sales_train=pd.read_csv("../input/sales_train.csv")
+    items=pd.read_csv("../input/items.csv")
+    shops = pd.read_csv("../input/shops.csv")
+    test = pd.read_csv("../input/test.csv")
+    item_categories = pd.read_csv("../input/item_categories.csv")
+
+    # print sales_train.head()
+    # print items.head()
+    # print shops.head()
+    # print test.head()
+    print item_categories.head()
+
+if __name__=='__main__':
+    main()

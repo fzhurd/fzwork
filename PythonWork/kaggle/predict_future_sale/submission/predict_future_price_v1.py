0bc7c6fb794ba45fdd5d123203556ab5a3715e34
fzhurd <minganghz@gmail.com>
2018-04-22 23:40:40 -0700

    add date time

--- PythonWork/kaggle/predict_future_sale/submission/predict_future_price_v1.py
+++ PythonWork/kaggle/predict_future_sale/submission/predict_future_price_v1.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 def main():
-    sales_train=pd.read_csv("../input/sales_train.csv")
+    sales_train=pd.read_csv("../input/sales_train_100.csv")
     items=pd.read_csv("../input/items.csv")
     shops = pd.read_csv("../input/shops.csv")
     test = pd.read_csv("../input/test.csv")
@@ -14,13 +14,21 @@ def main():
     print sales_train.head()
     print sales_train.tail()
 
-
     print sales_train.shape
     print sales_train.info()
     # print items.head()
     # print shops.head()
     # print test.head()
     # print item_categories.head()
+
+    sales_train['date']=sales_train.apply(pd.to_datetime, axis=1 )
+    print sales_train.head()
+    print sales_train.tail()
+
+
+def convert_to_date(date_str):
+    pass
+
 
 if __name__=='__main__':
     main()

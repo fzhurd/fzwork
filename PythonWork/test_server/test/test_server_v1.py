29e16320fe198420a293830e068062c827089dce
frank2 <minganghz@gmail.com>
2018-08-04 10:44:45 -0700

    add server

--- PythonWork/test_server/test/test_server_v1.py
+++ PythonWork/test_server/test/test_server_v1.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-print ('test')
+import jsonrpc2
+
+print 'test'
+
+# pip install jsonrpyc
+# export PYTHONPATH=$PYTHONPATH
+# runjsonrpc2 test_server_v1
+
+def greeting(name):
+    return dict(message="test, %s" %name)

e7e0a570c3be95e8aa8d6b0b11cb2dcf0bea64e3
fzhurd <minganghz@gmail.com>
2018-09-01 20:54:48 -0700

    add

--- PythonWork/kaggle/forest_cover_type/submission/predict_forest_cover_type_v1.py
+++ PythonWork/kaggle/forest_cover_type/submission/predict_forest_cover_type_v1.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-print 'test'
+import numpy as np
+import pandas as pd
+
+
+data_raw_train = pd.read_csv('../input/train.csv')
+data_raw_test = pd.read_csv('../input/test.csv')
+
+print data_raw_train.shape
+
+print data_raw_train.head()
+print data_raw_train.columns
+
+# print data_raw_train.isnull().sum()
+# print data_raw_test.isnull().sum()
+
+# print data_raw_test.columns
+
+print data_raw_train['Cover_Type'].value_counts()

0aaf38f19db804ede66b35c882a17986a230fa6e
fzhurd <minganghz@gmail.com>
2018-09-02 09:04:39 -0700

    add the importance analysis

--- PythonWork/kaggle/forest_cover_type/submission/predict_forest_cover_type_v1.py
+++ PythonWork/kaggle/forest_cover_type/submission/predict_forest_cover_type_v1.py
@@ -19,3 +19,47 @@ print data_raw_train.columns
 # print data_raw_test.columns
 
 print data_raw_train['Cover_Type'].value_counts()
+print data_raw_train.dtypes
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+
+
+features = ['Id', 'Elevation', 'Aspect', 'Slope',
+       'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology',
+       'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon',
+       'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points',
+       'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3',
+       'Wilderness_Area4', 'Soil_Type1', 'Soil_Type2', 'Soil_Type3',
+       'Soil_Type4', 'Soil_Type5', 'Soil_Type6', 'Soil_Type7',
+       'Soil_Type8', 'Soil_Type9', 'Soil_Type10', 'Soil_Type11',
+       'Soil_Type12', 'Soil_Type13', 'Soil_Type14', 'Soil_Type15',
+       'Soil_Type16', 'Soil_Type17', 'Soil_Type18', 'Soil_Type19',
+       'Soil_Type20', 'Soil_Type21', 'Soil_Type22', 'Soil_Type23',
+       'Soil_Type24', 'Soil_Type25', 'Soil_Type26', 'Soil_Type27',
+       'Soil_Type28', 'Soil_Type29', 'Soil_Type30', 'Soil_Type31',
+       'Soil_Type32', 'Soil_Type33', 'Soil_Type34', 'Soil_Type35',
+       'Soil_Type36', 'Soil_Type37', 'Soil_Type38', 'Soil_Type39',
+       'Soil_Type40']
+
+X = data_raw_train[features]
+y = data_raw_train['Cover_Type']
+
+# the other method to check the distribution of one feature
+print np.unique(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+
+importances = rf.feature_importances_
+print importances
+
+feat_labels = data_raw_train.columns[:-1]
+
+indicies = np.argsort(importances)[::-1]
+
+for f in xrange(X_train.shape[1]):
+    print feat_labels[indicies[f]], importances[indicies[f]]
+
+

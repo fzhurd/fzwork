d2b3446bcba9d4d48ad9cc64978a29b5d33aa40a
frank2 <minganghz@gmail.com>
2019-01-08 20:25:27 -0800

    train and validate data

--- PythonWork/kaggle/EnvPython3/predict_future_sales/submission/predict_future_sales_v2.py
+++ PythonWork/kaggle/EnvPython3/predict_future_sales/submission/predict_future_sales_v2.py
@@ -203,12 +203,20 @@ print (test_data.shape)
 print (test_data.head())
 
 print ('*'*30, 'validation data', '*'*30)
-dates = all_data['date_block_num']
-train_date = [ d for d in dates if d not in [30, 31, 32, 33]]
-val_date = [dates.isin([30, 31, 32, 33])]
+# dates = all_data['date_block_num']
+# train_date = [ d for d in dates if d not in [30, 31, 32, 33]]
+# val_date = [dates.isin([30, 31, 32, 33])]
 
-print ('train date: ', set(train_date))
-print ('val date: ', val_date)
+# print ('train date: ', set(train_date))
+# print ('val date: ', val_date)
+
+train_data = all_data.loc[~all_data['date_block_num'].isin([30, 31, 32, 33])]
+train_data = train_data.drop(to_drop_cols, axis=1)
+print (train_data.shape)
+print (train_data.columns)
+
+val_data = all_data.loc[all_data['date_block_num'].isin ([30, 31, 32, 33])]
+val_data = val_data.drop(to_drop_cols, axis=1)
 
 
 

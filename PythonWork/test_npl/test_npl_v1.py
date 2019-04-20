fefe23d02c6300c636d1b60daa355d5d200a38c2
fzhurd <minganghz@gmail.com>
2019-04-19 20:09:29 -0400

    add first version

---
+++ PythonWork/test_npl/test_npl_v1.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 14:31:28 2019
+
+@author: Frank2
+"""
+
+import keras
+import tensorflow
+from gensim.models import Word2Vec
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+import os
+import urllib
+import zipfile
+import tensorflow as tf
+
+url = 'http://mattmahoney.net/dc/'
+filename = maybe_download('text8.zip', url, 31344016)
+
+# Read the data into a list of strings.
+def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words."""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+vocabulary = read_data(filename)
+print(vocabulary[:7])
+
+
+vocab_size = 10000
+data, count, dictionary, reverse_dictionary = read_data(vocabulary_size=vocab_size)
+
+sampling_table = sequence.make_sampling_table(vocab_size)
+couples, labels = skipgrams(data, vocab_size, window_size=window_size, sampling_table=sampling_table)
+word_target, word_context = zip(*couples)
+word_target = np.array(word_target, dtype="int32")
+word_context = np.array(word_context, dtype="int32")
+
+print(couples[:10], labels[:10])

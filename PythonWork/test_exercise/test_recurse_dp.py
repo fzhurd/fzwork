db06838e1e7d0ecb6673d33e40a5ddc7457f1f3e
fzhurd <minganghz@gmail.com>
2019-12-08 11:18:32 -0500

    add mine gold test

--- PythonWork/test_exercise/test_recurse_dp.py
+++ PythonWork/test_exercise/test_recurse_dp.py
@@ -271,6 +271,97 @@ def calculate_bell_1(num):
 res =calculate_bell_1(5)
 print (res)
 
+def calculate_binom_coef(n, k):
+    
+#    C(n, k) = C(n-1, k-1) + C(n-1, k)
+#    C(n, 0) = C(n, n) = 1
+    if k==0 or k==n:
+        return 1
+    else:
+        temp = calculate_binom_coef(n-1, k-1) + calculate_binom_coef(n-1, k)
+        return temp
+
+res = calculate_binom_coef(4, 2)
+print (res)
+
+def coin_change(S, m, n):
+    if n < 0: 
+        return 0
+    if n==0:
+        return 1
+    if m <=0 and n >= 1: 
+        return 0
+    return coin_change( S, m - 1, n ) + coin_change( S, m, n-S[m-1] )
+
+
+
+
+# https://www.geeksforgeeks.org/gold-mine-problem/
+MAX = 100
+  
+# Returns maximum amount of 
+# gold that can be collected 
+# when journey started from 
+# first column and moves 
+# allowed are right, right-up 
+# and right-down 
+def mine_gold(gold, m, n):
+    # Create a table for storing 
+    # intermediate results 
+    # and initialize all cells to 0. 
+    # The first row of 
+    # goldMineTable gives the 
+    # maximum gold that the miner 
+    # can collect when starts that row 
+    goldTable = [[0 for i in range(n)] 
+                        for j in range(m)] 
+  
+    for col in range(n-1, -1, -1): 
+        for row in range(m): 
+  
+            # Gold collected on going to 
+            # the cell on the rigth(->) 
+            if (col == n-1): 
+                right = 0
+            else: 
+                right = goldTable[row][col+1] 
+  
+            # Gold collected on going to 
+            # the cell to right up (/) 
+            if (row == 0 or col == n-1): 
+                right_up = 0
+            else: 
+                right_up = goldTable[row-1][col+1] 
+  
+            # Gold collected on going to 
+            # the cell to right down (\) 
+            if (row == m-1 or col == n-1): 
+                right_down = 0
+            else: 
+                right_down = goldTable[row+1][col+1] 
+  
+            # Max gold collected from taking 
+            # either of the above 3 paths 
+            goldTable[row][col] = gold[row][col] + max(right, right_up, right_down) 
+                                                             
+    # The max amount of gold 
+    # collected will be the max 
+    # value in first column of all rows 
+    res = goldTable[0][0] 
+    for i in range(1, m): 
+        res = max(res, goldTable[i][0]) 
+  
+    return res 
+
+gold = [[1, 3, 1, 5], 
+    [2, 2, 4, 1], 
+    [5, 0, 2, 3], 
+    [0, 6, 1, 2]] 
+  
+m = 4
+n = 4
+  
+print(mine_gold(gold, m, n)) 
     
 
 
